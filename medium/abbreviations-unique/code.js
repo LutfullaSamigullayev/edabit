65754f353b57e6e947f3f3aacbe19644067cc7d6
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-07 17:07:05 +0500

    medium abbreviations-unique

--- medium/abbreviations-unique/code.js
+++ medium/abbreviations-unique/code.js
@@ -1,5 +1,26 @@
 function uniqueAbbrev(abbs, words) {
-  // Your code here.
+  // Har bir qisqartma uchun so'zlar massividan mos keladigan so'zlarni sanaymiz
+  for (let abb of abbs) {
+    let match_count = 0;
+    
+    for (let word of words) {
+      // So'z qisqartma bilan boshlanadimi?
+      if (word.startsWith(abb)) {
+        match_count += 1;
+      }
+    }
+
+    // Agar qisqartma bir nechta so'zni aniqlasa, noaniqlik bor
+    if (match_count > 1) {
+      return false;
+    }
+  }
+  
+  // Agar barcha qisqartmalar unikal bo'lsa
+  return true;
 }
+
+console.log(uniqueAbbrev(["s", "t", "v"], ["stamina", "television", "vindaloo"])); // true
+console.log(uniqueAbbrev(["ho", "h", "ha"], ["house", "hope", "happy"])); // false
 
 module.exports = uniqueAbbrev;
