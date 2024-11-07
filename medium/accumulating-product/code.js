fb25192e0494c46e1cf3e7e3b2608768651920c0
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-07 18:45:04 +0500

    medium accumulatingProduct

--- medium/accumulating-product/code.js
+++ medium/accumulating-product/code.js
@@ -1,6 +1,12 @@
 function accumulatingProduct(arr) {
   // Your code here.
-  
+  let result = []     // oldin javoblarni push qilib borish uchun massiv ochamiz
+  let sum = 1         // va sonlarni ko'paytirib borish uchun o'zgaruvchi ochamiz
+  arr.map((num) => {  // har bir itemni map qilamiz
+    sum *= num        // kelgan num ni sum ko'paytirib boramiz
+    result.push(sum)  // ko'paytmani push qilib boramiz
+  })
+  return result       //
 }
 
 module.exports = accumulatingProduct;
