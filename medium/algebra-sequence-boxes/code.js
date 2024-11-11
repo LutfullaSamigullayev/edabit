41c8675650a909ef89f993708fc379483727b6fb
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-11 12:35:08 +0500

    medium algebra-sequence-boxes

--- medium/algebra-sequence-boxes/code.js
+++ medium/algebra-sequence-boxes/code.js
@@ -1,5 +1,16 @@
 function boxSeq(step) {
-  // Your code here.
+  let boxes = 0;  // Boshlang'ich 0 ta quti
+    
+    for (let i = 1; i <= step; i++) {
+      console.log(boxes)
+        if (i % 2 !== 0) {  // Agar qadam toq bo'lsa
+            boxes += 3;
+        } else {  // Agar qadam juft bo'lsa
+            boxes -= 1;
+        }
+    }
+    
+    return boxes;
 }
 
 module.exports = boxSeq;
