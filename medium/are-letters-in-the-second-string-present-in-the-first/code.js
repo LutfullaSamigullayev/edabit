f6da3b758c821866171984c4bd8a864acecd31ec
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-25 09:55:40 +0500

    medium are-letters-in-the-second-string-present-in-the-first

--- medium/are-letters-in-the-second-string-present-in-the-first/code.js
+++ medium/are-letters-in-the-second-string-present-in-the-first/code.js
@@ -1,5 +1,10 @@
 function letterCheck(arr) {
   // Your code here.
+  return arr[1].split('').every((i) => arr[0].toLowerCase().includes(i.toLowerCase()))
+  // arr[1] ni split qilib so'zni harflarga ajatdik, keyin every qilib testladik,
+  // har birini arr[0] element ichidan qidirdik ikkala tarafni ham kichik harflarga aylantirib,
+  // agar barchasi testdan o'tsa true qaytaradi aks holda false
 }
+console.log(letterCheck(["trances", "nectar"]))
 
 module.exports = letterCheck;
