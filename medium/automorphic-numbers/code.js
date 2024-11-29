cf1fa9b32e3346401b737211ff3fd208a92b537b
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-29 15:57:56 +0500

    medium automorphic-numbers

--- medium/automorphic-numbers/code.js
+++ medium/automorphic-numbers/code.js
@@ -1,5 +1,10 @@
 function isAutomorphic(n) {
   // Your code here.
+  const sum = n*n // oldin n ning kvadratini chiqarib olamiz
+  return sum.toString().endsWith(n)
+  // keyin javobni stringga o'girib endWith orqali oxiridan n ni qidiramiz
 }
 
 module.exports = isAutomorphic;
+
+console.log(isAutomorphic(8))
