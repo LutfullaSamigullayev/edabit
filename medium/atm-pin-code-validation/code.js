572003a45d7f47bcaa8562bd7dc7d5fa0a99b974
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-29 12:33:57 +0500

    medium atm-pin-code-validation

--- medium/atm-pin-code-validation/code.js
+++ medium/atm-pin-code-validation/code.js
@@ -1,5 +1,7 @@
 function validatePIN(pin) {
   // Your code here.
+  return (pin.length === 4 || pin.length === 6 ) &&  (/^\d+$/.test(pin))
+  // olding uzunligi 4 yoki 6 ga teng bo'lishi kerak va Regex orqali barchasi raqam ekanligini tekshiradi
 }
 
 module.exports = validatePIN;
