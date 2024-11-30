c17d3814fb170a14c28d59e8fa13e29b033c2a1b
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-30 17:09:54 +0500

    medium barbecue-skewers

--- medium/barbecue-skewers/code.js
+++ medium/barbecue-skewers/code.js
@@ -1,5 +1,11 @@
 function bbqSkewers(grill) {
   // Your code here.
+  let o = 0 // o larni hisoblab borish uchun o'zgaruvchi ochamiz
+  let x = 0 // x larni hisoblab borish uchun o'zgaruvchi ochamiz
+  grill.forEach(e => { // grill ni foreach qilib har biri stringni tekshiramiz
+    e.includes('x') ? x++ : o++ // agar e ning ichida x bo'lsa x ni 1 ga oshiramiz 
+  });       // aks holda o ni 1 ga oshiramiz
+  return [o, x]   // javobni massiv ko'rinishida qaytaramiz
 }
 
 module.exports = bbqSkewers;
