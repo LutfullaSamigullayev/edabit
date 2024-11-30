2879c7f87a7a32b7590cd3abe4af208314d2a18b
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-30 18:07:06 +0500

    medium basic-e-mail-validation

--- medium/basic-e-mail-validation/code.js
+++ medium/basic-e-mail-validation/code.js
@@ -1,5 +1,13 @@
 function validateEmail(str) {
   // Your code here.
+  let e = 0 //bu yerda biz '@' belgini index ini saqlaymiz
+  if(str.includes('@') && str.includes('.') && (str.indexOf('@') < str.lastIndexOf('.'))) {
+    // '@' borligini va '.' borligini va '@' ning joylashgan joyi '.' dan oldin bo'lishini tekshiramiz
+    e = str.indexOf('@') // e ga '@' ni indexini joylashtiramiz
+    if(str.charAt(e-1)) { // '@' ning joylashgan joyidan oldingi indexda element borligini tekshiramiz
+      return true // shularning barchasi to'gri bo'lsa true qaytaramiz
+    } else {return false} // aks holda false
+  } else {return false} // oldingi shartlarga ham to'gri kelmasa false qaytaramiz
 }
 
 module.exports = validateEmail;
