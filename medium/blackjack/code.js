d3f0b999424e54f24a96fef1fd2c04aae9ab37bc
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-03 16:37:30 +0500

    medium blackjack

--- medium/blackjack/code.js
+++ medium/blackjack/code.js
@@ -1,5 +1,22 @@
 function overTwentyOne(cards) {
   // Your code here.
+  let summ = 0
+  let ace = 0
+  cards.map((card) => {
+    if(typeof card === 'number') {
+      summ += card
+    } else if(card === 'J' || card === 'Q' ||card === 'K') {
+      summ += 10
+    } else if(card === 'A') {
+      summ += 11
+      ace ++
+    }
+  })
+  while(summ > 21 && ace > 0) {
+    summ -= 10
+    ace --
+  }
+return summ > 21 ? true : false
 }
 
 module.exports = overTwentyOne;
