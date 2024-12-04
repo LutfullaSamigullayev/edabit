fb2c5e94e4102513bafb1756b12e6b8eaea93219
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 15:14:02 +0500

    medium check-if-one-array-is-subset-of-another

--- medium/check-if-one-array-is-subset-of-another/code.js
+++ medium/check-if-one-array-is-subset-of-another/code.js
@@ -1,5 +1,9 @@
 function subset(arr1, arr2) {
   // Your code here.
+  return arr1.every((e) => arr2.includes(e)) 
+  // every har doim true bo'lsa yana ishlaydi biz arr1 dagi raqamlarni arr2 dan qidiramiz
+  // agar barchasi topilsa true qaytaramiz
+  // qaysidir birsi topilmay qolsa shu yerda ish to'xtab false qaytadi
 }
 
 module.exports = subset;
