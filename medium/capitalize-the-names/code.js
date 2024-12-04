7c3e5fb4fa1fc394681bc4b586ea30d17af58ea1
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 13:26:40 +0500

    medium capitalize-the-names

--- medium/capitalize-the-names/code.js
+++ medium/capitalize-the-names/code.js
@@ -1,5 +1,10 @@
 function capMe(arr) {
   // Your code here.
+  let res = [] // bu yerga javoblarni push qilib boramiz
+  arr.map((e) => { // keyim arr ni map qilib har bir elementning 0-indexini katta harf qilamiz
+    res.push(e[0].toUpperCase()+e.slice(1).toLowerCase()) // va qolgan harflarni kichik harf qilib
+  })  // ikkalasini qo'shib bitta satr qilib res ga push qilib boramiz
+  return res  // keyin res ni return qilamiz
 }
 
 module.exports = capMe;
