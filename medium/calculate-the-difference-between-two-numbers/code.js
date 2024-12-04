b3c49565a10ccdcea9e7b56ea2a37dcf1af25cae
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 11:59:14 +0500

    medium calculate-the-difference-between-two-numbers

--- medium/calculate-the-difference-between-two-numbers/code.js
+++ medium/calculate-the-difference-between-two-numbers/code.js
@@ -1,5 +1,9 @@
 function percentDiff(num1, num2) {
   // Your code here.
+  const res = Math.abs(num1 - num2)/((num1+num2)/2)*100 // Foizli farq formulasi
+  if(Number.isInteger(res)){  // agar res butun son bo'lsa
+    return res    // uning o'zini qaytaramiz
+  } else return +res.toFixed(1) // aks holda uni bir onlik raqamgacha yaxtlitlaymiz
 }
 
 module.exports = percentDiff;
