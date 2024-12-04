74a2e9c08b16b6a946dbeab8749480573e9cad7c
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 15:06:55 +0500

    medium check-if-a-number-is-prime

--- medium/check-if-a-number-is-prime/code.js
+++ medium/check-if-a-number-is-prime/code.js
@@ -1,5 +1,15 @@
 function isPrime(num) {
   // Your code here.
+  if (num <= 1) {
+    return false; // 1 va manfiy sonlar tub son emas
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+        return false; // Agar son i ga bo'linadigan bo'lsa, tub son emas
+    }
+  }
+  return true; // Agar hech bo'linuvchi topilmasa, tub sondir
+
 }
 
 module.exports = isPrime;
