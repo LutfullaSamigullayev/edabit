57d8400434def0a082f3de5120eb0d17dd6b137e
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 13:19:36 +0500

    medium capitalize-the-first-letter-of-each-word

--- medium/capitalize-the-first-letter-of-each-word/code.js
+++ medium/capitalize-the-first-letter-of-each-word/code.js
@@ -1,5 +1,12 @@
 function makeTitle(str) {
   // Your code here.
+  let arr = str.split(' ') // oldin str ni bo'laklarga bo'lib olamiz
+  let newarr = [] // bu yerga javobni push qilib boramiz
+  arr.map((e) => {  // bo'laklarga bo'lingan so'zlar ustida amallar bajaramiz
+    newarr.push(e[0].toUpperCase()+e.slice(1))  // har bir so'zning 0 indexdagisini katta qilib keyin qolgan 1-indexdan boshlab qolgan so'zlarni qo'shib 
+  })  // newarr ga push qilib boramiz
+  let res = newarr.join(' ')  // keyin newarr ni join qilib string ga o'zgartiramiz
+  return res
 }
 
 module.exports = makeTitle;
