7fa77f84e219a75943c5610a7d735a85a12efc7c
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 15:58:59 +0500

    medium check-if-the-string-is-a-palindrome

--- medium/check-if-the-string-is-a-palindrome/code.js
+++ medium/check-if-the-string-is-a-palindrome/code.js
@@ -1,5 +1,12 @@
 function isPalindrome(str) {
   // Your code here.
+  let newstr = str.toLowerCase().replace(/[ ,!-]/g, '')
+  // str ni kichik harflarga aylantirib undagi ortiqcha belgilarni olib tashlaymiz
+  let str1 = newstr.slice(0, Math.round(newstr.length/2))
+  // satrning yarimini qirqib olamiz
+  let str2 = newstr.split('').reverse().join('').slice(0, Math.round(newstr.length/2))
+  // keyin satrni bo'laklarga bo'lib teskarisiga aylantirib yana qo'shib, uning ham yarimini olamiz
+  return str1 === str2 // agar str1 va str2 bir xil bo'lsa true qaytaradi aks holda false qaytaradi
 }
 
 module.exports = isPalindrome;
