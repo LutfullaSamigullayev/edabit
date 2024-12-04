d21dc76bd48ab3938c060685f8a2e92eafb63e4e
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 14:43:19 +0500

    medium check-for-anagrams

--- medium/check-for-anagrams/code.js
+++ medium/check-for-anagrams/code.js
@@ -1,5 +1,9 @@
 function isAnagram(s1, s2) {
-  // Your code here.
+  str1 = s1.toLowerCase().replace(/\s/g, '').split('').sort().join('')
+  str2 = s2.toLowerCase().replace(/\s/g, '').split('').sort().join('')
+  // barcha harflarni kichik qilamiz keyin bosh joylarni olib tashlaymiz
+  // keyin bo'laklarga bo'lib sortlab yana birlahstirib qo'yamiz
+  return  str1 === str2 // agar ular bir xil bo'lsa true aks holda false qaytadi
 }
 
 module.exports = isAnagram;
