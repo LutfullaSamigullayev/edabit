1e2a98599796b3ec1f810ada00d1e32464b60c33
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-04 16:28:27 +0500

    medium chocolate-dilemma

--- medium/chocolate-dilemma/code.js
+++ medium/chocolate-dilemma/code.js
@@ -1,5 +1,15 @@
 function testFairness(agatha, bertha) {
   // Your code here.
+    
+  function result(arr) { // yigindini hisoblab beruvchi bir funksiya yaratamiz
+    let sum = 0 // yigindini qo'shib borish ucun sum o'zgaruvchi ochamiz
+    arr.forEach((inArr) => { // promt dan kelgan arr ni forEach qilib har bir kelgan inArr ustida amal bajaramiz
+      sum += inArr.reduce((a,b) => a*b) // inArr ni ham reduce qilib ichidagi sonlarni qo'shib
+    }) // sumga yig'ib boramiz
+    return sum // keyin sum ni return qilamiz
+  }
+  // agar agatha va bertha ning sonlar yig'indisi teng bo'lsa true qaytaramiz aks holda false
+  return result(agatha) === result(bertha) 
 }
 
 module.exports = testFairness;
