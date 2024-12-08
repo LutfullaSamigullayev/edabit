0573b1786f950517c3d690bf466f91e9c58df8cd
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-08 20:58:17 +0500

    medium consecutive-numbers

--- medium/consecutive-numbers/code.js
+++ medium/consecutive-numbers/code.js
@@ -1,5 +1,14 @@
 function cons(arr) {
   // Your code here.
+  const newarr =  arr.sort((a,b) => (a-b)) // oldin arrni so'rtlab olamiz
+  // keyin newarr ni every qilib xar birini tekshiramiz 
+  return newarr.every((num, i, arr) => { //num, index, va arr ni olamiz
+    if(i+1 != newarr.length){ // agar bu oxirgi element bo'lmasa uni tekshiramiz
+     return (num + 1) === arr[i+1]  //kelgan num ga 1 ni qo'shib ozidan oldingai num ni tengligini tekshiramiz
+    } else {  // agar teng bo'lsa true qaytaradi
+      return true // agar u oxirgi element bo'lsa uni tekshirmay true qaytaradi
+    }
+}) // agar barchasi shartni bajarsa true qaytaradi 
 }
 
 module.exports = cons;
