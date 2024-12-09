37ee7c310a9007c5171b97b6221f6e6baf53c194
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-09 20:51:46 +0500

    medium convenience-store

--- medium/convenience-store/code.js
+++ medium/convenience-store/code.js
@@ -1,5 +1,9 @@
 function changeEnough(change, amountDue) {
   // Your code here.
+  const res = (change[0]*0.25) + (change[1]*0.10) + (change[2]*0.05) + (change[3]*0.01)
+  // 0-indexni 0.25 ga, 1-indexni 0.10 ga, 2-indexni 0.05 ga va 3-indexni 0.01 ga ko'paytirib
+  // cho'ntakda bo'r bo'lgan summani chiqaramiz keyin
+  return res >= amountDue // cho'ntakdagi pul mahsulot narxiga yetsa true qaytaradi
 }
 
 module.exports = changeEnough;
