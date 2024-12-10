92bcb87d7f04fe05fbd4789bca244e587b0e64bc
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 10:22:15 +0500

    medium convert-zero-and-one-text-to-1-and-0

--- medium/convert-to-hex/code.js
+++ medium/convert-to-hex/code.js
@@ -1,10 +1,14 @@
 function toHex(str) {
   // Your code here.
-  let res = ''
-  for(let i = 0; i < str.length; i++) {
-    res += str.charCodeAt(i).toString(16) + ' '
+  let res = '' // javoblarni saqlab borish uchun bo'sh string ochib qo'yamiz
+  for(let i = 0; i < str.length; i++) { 
+    // keyin for qilib str ni har bir elenenti ustida amal bajaramiz
+    res += str.charCodeAt(i).toString(16) + ' ' 
+    // charCodeAt orqali o'sha indexda turgan elementni codini olib keyin 
+    // uni toString(16) orqali 16-lik sanoq sistemasiga o'tkazamiz 
+    // va bo'sh joy bilan res ga qo'shib boramiz
   }
-  return res.trimEnd()
+  return res.trimEnd() // va  oxida qo'shib yuborgan bo'sh joyimizni olib tashlaymiz 
 }
 
 console.log(toHex("hello world"))
--- medium/convert-zero-and-one-text-to-1-and-0/code.js
+++ medium/convert-zero-and-one-text-to-1-and-0/code.js
@@ -1,5 +1,19 @@
 function textToNumberBinary(str) {
-  // Your code here.
+  const words = str.toLowerCase().split(' ');
+  // oldin str ni kichik harflarga aylantirib uni bo'sh joylar orqali bo'laklarga bo'lib chiqamiz
+  let res = ''; // bu yerda jovoblarni saqlab boramiz
+  for (let word of words) { // keyin for qilib har birini tekshirib chiqamiz
+    if (word === 'zero') {  // agar word zero ga teng bo'lsa
+      res += '0'; // res ga 0 ni string qilib qo'shamiz
+    } else if (word === 'one') { // aks holda word noe ga teng bo'lsa
+      res += '1'; //res ga 1 ni string qilib qo'shamiz
+    } // aks holda hechnima qo'shilmaydi
+  }
+  const length = res.length; // javob tayyor bo'lganidan keyin uning uzunligini tekshiramiz
+  if (length % 8 !== 0) { // uni 8 karrali ekanligini aniqlaymiz agar uni 8 ga bo'lganda qoldiq qolsa
+    res = res.slice(0, length - (length % 8));  // qolgan qoldiqni res ning oziridan kesib tashlaymiz
+  } // agar uzunlik 8 dan kichik bo'lsa uni uzunlikning o'zi qaytadi va barcha uzunliki kesib tashlanidi
+  return res; // keyin natijani return qilamiz
 }
-
+console.log(2%8)
 module.exports = textToNumberBinary;
