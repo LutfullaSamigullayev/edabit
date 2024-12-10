5f47d5b29c35dd10ec5867e9f3c75c624ed53096
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 08:57:58 +0500

    medium convert-to-hex

--- medium/convert-to-hex/code.js
+++ medium/convert-to-hex/code.js
@@ -1,5 +1,11 @@
 function toHex(str) {
   // Your code here.
+  let res = ''
+  for(let i = 0; i < str.length; i++) {
+    res += str.charCodeAt(i).toString(16) + ' '
+  }
+  return res.trimEnd()
 }
 
+console.log(toHex("hello world"))
 module.exports = toHex;
