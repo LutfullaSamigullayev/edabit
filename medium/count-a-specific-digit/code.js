cf315723e3035edfbbc2a55a1b819e2abd1b278a
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 10:57:10 +0500

    medium count-a-specific-digit

--- medium/count-a-specific-digit/code.js
+++ medium/count-a-specific-digit/code.js
@@ -1,5 +1,12 @@
 function digitOccurrences(min, max, digit) {
   // Your code here.
-}
+  let summ = 0 // bu yerga javob larni qo'shib boramiz
+  let str = ''  // bu yerga raqamlarni yig'ib chiqamiz
+  for(let num = min; num <= max; num++) { // min dan max gacha bo'lgan sonlarni
+    str += Math.abs(num)  // str ga musbat son sifatida yig'ib boramiz
+  } // keyin str ni split qilib bo'laklarga bo'lib forEach qilamiz 
+  str.split('').forEach((num) => +num === digit ? summ++ : null) 
+  return summ // agar num digit ga teng bo'lsa summ ni 1 ga oshirib boramiz 
+} // keyin summ ni return qilamiz
 
 module.exports = digitOccurrences;
