276fc328f9421554eb1b7a6589a367fec3631efd
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 11:06:41 +0500

    medium count-letters-in-a-word-search

--- medium/count-letters-in-a-word-search/code.js
+++ medium/count-letters-in-a-word-search/code.js
@@ -1,5 +1,10 @@
 function letterCounter(arr, letter) {
   // Your code here.
+  let summ = 0 // bu yerga javoblarni qo'shib boramiz
+  arr.forEach(arr => {  // oldin katta arr ni forEach qilamiz keyin uni ichidagi
+    arr.forEach((e) => e === letter ? summ++ : null) // arr ni forEach qilamiz
+  }); // e === letter true bo'lsa summ ni 1 ga oshiramiz aks hooloda hechnima qilinmaydi
+  return summ // summ ni return qilamiz
 }
 
 module.exports = letterCounter;
