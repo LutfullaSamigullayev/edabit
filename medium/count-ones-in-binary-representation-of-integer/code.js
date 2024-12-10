fadd52c179117e9b7080d24962d2f7cd82aa2a4d
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 11:49:17 +0500

    medium count-ones-in-binary-representation-of-integer

--- medium/count-ones-in-binary-representation-of-integer/code.js
+++ medium/count-ones-in-binary-representation-of-integer/code.js
@@ -1,5 +1,9 @@
 function countOnes(i) {
   // Your code here.
+  let binary = i.toString(2) // oldin kelgan raqanmi 2-lik sanoq sistemasiga o'tkazamiz
+  let res = binary.split('').filter((num) => +num === 1)
+  //keyin uni bo'laklarga bo'lib filter qilib faqat 1 ga tenglarni olamiz
+  return res.length // keyin 1 larning uzunligini chiqaramiz
 }
 
 module.exports = countOnes;
