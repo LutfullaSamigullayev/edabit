a573e1c3e727f6516fd37e84285afce0fcf54d5d
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-10 08:32:13 +0500

    medium convert-key-values-in-an-object-to-array

--- medium/convert-key-values-in-an-object-to-array/code.js
+++ medium/convert-key-values-in-an-object-to-array/code.js
@@ -1,5 +1,7 @@
 function objectToArray(obj) {
   // Your code here.
+  return Object.entries(obj); 
+  // obyektning entries property si obyektni array qilib beradi
 }
 
 module.exports = objectToArray;
