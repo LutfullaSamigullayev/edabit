f85cfc3eb84cba0d5289e11bcb34c103001aea59
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-11 14:19:37 +0500

    medium cumulative-array-sum

--- medium/cumulative-array-sum/code.js
+++ medium/cumulative-array-sum/code.js
@@ -1,5 +1,11 @@
 function cumulativeSum(arr) {
   // Your code here.
+  const newarr = arr.map((num, index, arr) => { // arr map qilamiz
+    arr[index+1] = num + arr[index+1]
+    // num ni o'zidan oldingi indexga qo'shib javobni o'zidan oldingi indexga joylashtirib boramiz
+    return num //kelgan num ni newarr ga return qilib boramiz
+  })
+  return newarr
 }
 
 module.exports = cumulativeSum;
