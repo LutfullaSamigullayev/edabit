4250cc751dcc0e0e2ae69123d8be87a5e5a17498
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-11 13:02:15 +0500

    medium counting-adverbs

--- medium/count-the-smiley-faces/code.js
+++ medium/count-the-smiley-faces/code.js
@@ -1,20 +1,20 @@
 function countSmileys(arr) {
   // Your code here.
-  let smiles = 0
-  arr.forEach(smile => {
-    let face = 0
-    if(smile[0] === ":" || smile[0] === ";") {
-      face++
+  let smiles = 0 // bu yerda smile larni saqlab boramiz
+  arr.forEach(smile => {  // keyin arr ni forEach qilib har smile larni tekshirib chiqamiz
+    let face = 0  // bu yerda ko'z burun va og'izlar barchasi shartga mos kelganligini hisoblab boramiz
+    if(smile[0] === ":" || smile[0] === ";") { 
+      face++ // agar ko'z shu shartlarga to'g'ri kelsa faceni 1 ga oshiramiz
     }
     if(smile.length === 3 && smile[1] === '-' || smile[1] === '~') {
-      face++
+      face++ // agar burun bo'lsa va shu shartlarga to'g'ri kelsa faceni 1 ga oshiramiz
     }
     if(smile[smile.length-1] === ')' || smile[smile.length-1] === 'D') {
-      face++
+      face++ // agar og'iz shu shartlarga to'g'ri kelsa faceni 1 ga oshiramiz
     }
-    face === smile.length && smiles++;
+    face === smile.length && smiles++; // face ya smile uzunligi mos kelsa smiles ni 1 ga oshiramiz
   })
-  return smiles
+  return smiles // keyin smiles ni return qilamiz
 }
 
 
--- medium/counting-adverbs/code.js
+++ medium/counting-adverbs/code.js
@@ -1,5 +1,16 @@
 function countAdverbs(sentence) {
   // Your code here.
+  let adverbs = 0 // bu yerda qo'shimchasi bor so'zlarni sonini hisoblab boramiz
+  sentence.split(' ').forEach(word => { 
+    // keyin matn ni split qilb bo'sh joylar orqali bo'laklarga bo'lib  forEach qilamiz
+    if(word.slice(-1) === ',' || word.slice(-1) === '.') {
+      // agar word ning oxirida nuqta yoki vergul bo'lsa uni olib tashlaymiz
+      word = word.slice(0, -1)
+    }
+    word.slice(-2) === 'ly' && adverbs++ 
+    // keyin so'zning oxirgi 2 ta harfi ly ga teng bo'lsa adverbs ni 1 ga oshiramiz
+  })
+  return adverbs // adverbsni return qilamiz
 }
 
 module.exports = countAdverbs;
