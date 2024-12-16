1f744c85471546170dd164f78e59abe8b710384a
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-16 11:43:35 +0500

    medium equality-of-3-values

--- medium/equality-of-3-values/code.js
+++ medium/equality-of-3-values/code.js
@@ -1,5 +1,12 @@
 function equal(a, b, c) {
   // Your code here.
+  if (a === b && b === c) { // agar barcha sonlar bir xil bo'lsa
+    return 3;  // 3 tasi ham bir xil deb chiqaramiz
+  } else if (a === b || b === c || a === c) { // agar ikki tasi bir xil bo'lsa
+    return 2;  // 2 tasi bir xil deb chiqaramiz
+  } else {
+    return 0;  // aks holda 0 chiqaramiz
+  }
 }
 
 module.exports = equal;
