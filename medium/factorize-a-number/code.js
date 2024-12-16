473e214fd75be77594ad3098763a661793be9038
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-16 12:38:33 +0500

    medium factorize-a-number

--- medium/factorize-a-number/code.js
+++ medium/factorize-a-number/code.js
@@ -1,5 +1,12 @@
 function factorize(num) {
   // Your code here.
+  const factors = [] // bu yerga num ni qaysi sonlarga bo'lsa bo'ladiganini qo'shib boramiz
+  for(let i = 1; i <= num; i++) { // 1 da num gacha foq qilamiz
+    if(num % i === 0) { // agar num ni kelgan songa bo'lganda 0 chiqsa
+      factors.push(i) // uni factors ga push qilib boramiz
+    }
+  } 
+  return factors  // keyin factors ni return qilamiz
 }
 
 module.exports = factorize;
