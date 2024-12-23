cd16b21df890a383831814e904b97bc8937f8ba9
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-12-23 12:43:30 +0500

    medium find-the-characters-counterpart-char-code

--- medium/find-the-characters-counterpart-char-code/code.js
+++ medium/find-the-characters-counterpart-char-code/code.js
@@ -1,5 +1,13 @@
 function counterpartCharCode(char) {
   // Your code here.
+  if(char.toLocaleLowerCase() === char) {
+    // agar harf uning kichiraytirilganiga teng bo'lsa demak u kichik harf
+   return char.toUpperCase().charCodeAt(0)
+   // shunda uni katta harf qilib codini return qilamiz
+  } else {
+    return char.toLowerCase().charCodeAt(0)
+    // aks holda uni kichik harf qilib kodini qaytaramiz
+  } 
 }
 
 module.exports = counterpartCharCode;
