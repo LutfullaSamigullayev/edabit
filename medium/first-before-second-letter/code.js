95da4cecd7e3ee9469ff4bf5ce438a27bb2791be
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-05 09:24:39 +0500

    medium first-before-second-letter

--- medium/first-before-second-letter/code.js
+++ medium/first-before-second-letter/code.js
@@ -1,5 +1,20 @@
 function firstBeforeSecond(s, first, second) {
   // Your code here.
+  let firstIndex = 0  // bu yerda birinchi berilgan harfni index ni saqlaymiz
+  let secondIndex = 0 // bu yerda ikkinchi berilgan harfni index ni saqlaymiz
+  s.split('').forEach((word, index) => {
+    // keyin s ni split qilib bo'laklarga bo'lib forEach qilamiz
+    if(word === first) {  
+      // agar kelgan harf first ga teng bo'lsa
+      firstIndex = index  
+    } // uning indexni firstIndexga o'zlashtiramiz
+    if(word === second && secondIndex === 0) {
+      // agar kelgan harf second ga teng bo'lsa va
+      // u hali birinchi marta uchrashi bo'lsa
+      secondIndex = index
+    } // uning indexni secondIndexga o'zlashtiramiz
+  }) 
+  return firstIndex < secondIndex
 }
 
 module.exports = firstBeforeSecond;
