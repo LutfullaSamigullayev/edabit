291194e172171cf901fb96eb871d2aefb0ae62ab
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-06 16:37:21 +0500

    medium fix-the-error-flattening-an-array

--- medium/fix-the-error-flattening-an-array/code.js
+++ medium/fix-the-error-flattening-an-array/code.js
@@ -1,5 +1,9 @@
 function flatten(arr) {
   // Your code here.
+  const res = [] // bu yerga javob ni saqlab boramiz
+  arr.forEach(item => item.forEach(item => res.push(item)))
+  // arr.ni for qilib uni ichidagi arr ni ham for har birini res ga push qilib chiqamiz
+  return res  // res ni return qilamiz
 }
 
 module.exports = flatten;
