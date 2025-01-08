ab4a87c013c81289be688e0d18dcfcbecce65571
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-08 13:29:13 +0500

    medium fix-the-error-value-vs-reference-types

--- medium/fix-the-error-value-vs-reference-types/code.js
+++ medium/fix-the-error-value-vs-reference-types/code.js
@@ -1,5 +1,10 @@
 function checkEquals(arr1, arr2) {
   // Your code here.
+  return arr1.every((num, index) => num === arr2[index])
+  // arr1 ni every qilib har bir num ni o'zgi jo'ylashgan joy bo'yichi arr2 
+  // bilan solishtirib chiqamiz agar hammasi bir xil bo'lsa true qaytadi
+  // aks holda bittasi to'gri kelmasa shu yerda ishini tugatadi
+  // va false qaytadi
 }
 
 module.exports = checkEquals;
