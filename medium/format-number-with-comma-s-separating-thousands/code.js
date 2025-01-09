c78b7f58d6b9b645b71410744d2856fcefc94b39
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-09 14:40:36 +0500

    medium format-number-with-comma-s-separating-thousands

--- medium/format-number-with-comma-s-separating-thousands/code.js
+++ medium/format-number-with-comma-s-separating-thousands/code.js
@@ -1,5 +1,18 @@
 function formatNum(num) {
-  // Your code here.
+  let str = String(num);  // Sonni stringga aylantiramiz
+  let result = ''; // bu yerga javob ni saqlab boramiz
+  let count = 0;  // bu yerda vergulni qayerga qo'yishni hisoblab boramiz
+  // Raqamlarni oxiridan boshlab for qilamiz
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = str[i] + result;  // oxirgi raqamdan qo'shib boramiz
+    count++; // for har ishlaganda 1 ga oshirib boramiz
+    // Har 3 raqamda vergul qo'shamiz
+    if (count % 3 === 0 && i !== 0) { 
+      //agar count 3 ga qoldiqsiz bo'linsa va i 0-index bo'lmasa
+      result = ',' + result; // result ga ',' qo'shamiz
+    }
+  }
+  return result;
 }
 
 module.exports = formatNum;
