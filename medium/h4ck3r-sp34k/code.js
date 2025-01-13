c3e44ad0ef1f62e2499c32d2fdcb21ce6c853020
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-13 18:15:05 +0500

    medium h4ck3r-sp34k

--- medium/h4ck3r-sp34k/code.js
+++ medium/h4ck3r-sp34k/code.js
@@ -1,5 +1,21 @@
 function hackerSpeak(str) {
   // Your code here.
+  const hackerCode = { // bu yerda harflarning kodi bo'ladi
+    a: '4',
+    e: '3',
+    i: '1',
+    o: '0',
+    s: '5',
+  };  
+  let res = '' // bu yerga javoblarni saqlab boramiz
+  str.split('').forEach(e => { // strni split qilib forEach qilib tekshirib chiqamiz
+    if(hackerCode[e]) { // agar kelgan element hackerCode da bo'lsa 
+      res += hackerCode[e]  // uning hackerCode dagi kodini saqlaymiz
+    } else {  // aks holda
+      res += e  // uning asl qiymatini saqlaymiz
+    }
+  })
+  return res // res ni return qilamiz
 }
 
 module.exports = hackerSpeak;
