73ce866462d7171905e55ef8fd5c8fa8e6b14663
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-13 08:56:05 +0500

    medium grab-the-city

--- medium/grab-the-city/code.js
+++ medium/grab-the-city/code.js
@@ -1,5 +1,18 @@
 function grabCity(str) {
   // Your code here.
+  let word = '' // bu yerga so'ngi massiv [] ichidagi so'zni saqlaymiz
+  let open = false  // agar true bo'lsa so'zni saqlashni boshlaymiz 
+  str.split('').forEach(e => { // strni split qilib forEach qilib tekshirib chiqamiz
+    if(e === '[') { // agar kelgan element '[' bo'lsa 
+      open = true   // open true qilamiz 
+      word = '' // va wordni tozlaymiz 
+    } else if (e === ']') { // agar kelgan element ']' bo'lsa 
+      open = false // open false qilamiz 
+    } else if (open) { // agar open true bo'lsa demak so'zni saqlash mumkin
+      word += e // shuning uchun kelgan elementni word ga saqlab boshlaymiz
+    } // toki open false bo'lgunicha
+  })
+  return word // keyin wordni return qilamiz
 }
 
 module.exports = grabCity;
