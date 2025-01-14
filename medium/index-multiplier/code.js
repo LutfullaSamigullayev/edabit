95a6e4c464167e4a811a0967e60959d6a618b56c
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-14 21:15:48 +0500

    medium index-multiplier

--- medium/index-multiplier/code.js
+++ medium/index-multiplier/code.js
@@ -1,5 +1,9 @@
 function indexMultiplier(arr) {
   // Your code here.
+  let res = 0 // bu yerda javoblarni qo'shib boramiz
+  arr.forEach((num, index) => res += num * index)
+  // arr ni forEach qilib num ni indexga ko'paytirib javobni res ga qo'shib boramiz
+  return res  // res ni return qilamiz
 }
 
 module.exports = indexMultiplier;
