453c8a9769ef18a6d76fd9a56d2bbc45b6011cc2
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-14 21:14:09 +0500

    medium hitting-the-jackpot

--- medium/hitting-the-jackpot/code.js
+++ medium/hitting-the-jackpot/code.js
@@ -1,5 +1,8 @@
 function testJackpot(result) {
   // Your code here.
+  return result.every(e => e === result[0])
+  // resultni every qilib har bir e ni result ning 0-elelmentiga solishtirib chiqamiz
+  // agar barchasi bir xil bo'lsa true qaytadi aks holda false
 }
 
 module.exports = testJackpot;
