14f61342c448db0ee59a729adf938d8364500698
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-17 15:37:59 +0500

    medium intersecting-intervals

--- medium/intersecting-intervals/code.js
+++ medium/intersecting-intervals/code.js
@@ -1,5 +1,13 @@
 function countOverlapping(intervals, point) {
   // Your code here.
+  let count = 0 // bu yerda javoblarni qo'shib boramiz
+  intervals.forEach(arr => {  // intervals ni forEach qilib har bir arr ni tekshiramiz
+    if(arr[0] <= point && point <=arr[1]){ 
+      // agar arr ning 0-elementi pointdan kichik bo'lsa va 1-elementi pointdan katta bo'lsa
+      count++ // count ni 1 ga oshiramiz
+    }
+  })
+  return count // count ni return qilamiz
 }
 
 module.exports = countOverlapping;
