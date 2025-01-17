ee9373a0bad116be939633878a5f495e1afc3d79
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-17 15:39:07 +0500

    medium is-johnny-making-progress

--- medium/is-johnny-making-progress/code.js
+++ medium/is-johnny-making-progress/code.js
@@ -1,5 +1,13 @@
 function progressDays(runs) {
   // Your code here.
+  let progress = 0  // bu yerda o'sish bo'lgan kunlar sonini hisoblab boramiz
+  runs.forEach((num, index, arr) => { // runs ni forEach qilib tekshiramiz
+    if(index != 0 && num > arr[index-1]) {  
+      // agar 0-index bo'lmasa va num o'zidan oldingi num dan katta bo'lsa
+      progress ++ // progress ni 1 ga oshiramiz
+    }
+  })
+  return progress // progress ni return qilamiz
 }
 
 module.exports = progressDays;
