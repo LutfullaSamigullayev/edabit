e20aa6fe565208970c967fdd2f1e2dc8606f90c3
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-18 21:22:57 +0500

    medium is-the-number-a-prime

--- medium/is-the-number-a-prime/code.js
+++ medium/is-the-number-a-prime/code.js
@@ -1,5 +1,15 @@
 function isPrime(num) {
   // Your code here.
+  if (num <= 1) return false; // 1 yoki 0 tub son emas
+  if (num <= 3) return true; // 2 va 3 tub sonlar
+  if (num % 2 === 0 || num % 3 === 0) return false; // 2 va 3 bilan bo'linmaslikni tekshirish
+
+  // 5 dan kvadrat ildiziga qadar bo'lgan sonlar bilan bo'linmasligini tekshirish
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+
+  return true; // Agar hech qaysi biriga bo'linmasa, tub son
 }
 
 module.exports = isPrime;
