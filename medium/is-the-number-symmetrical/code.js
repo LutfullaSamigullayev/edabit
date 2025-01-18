57c06356ad9d492fed4ac3cdb2e8fcb606c2a7f7
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-18 21:23:30 +0500

    medium is-the-number-symmetrical

--- medium/is-the-number-symmetrical/code.js
+++ medium/is-the-number-symmetrical/code.js
@@ -1,5 +1,10 @@
 function isSymmetrical(num) {
   // Your code here.
+  return +(num.toString().split('').reverse().join('')) === num
+  // num ni string qilib split orqali arr ga aylantirib va reverse bilan teskarisiga
+  // aylantirib yana qo'shib qo'yib stringga aylantiramiz
+  // va hosil bo'lgan teskari sonni string ko'rinishidan '+' orqali Number qilamiz
+  // va javobni num bilan solishtirib return qilamiz
 }
 
 module.exports = isSymmetrical;
