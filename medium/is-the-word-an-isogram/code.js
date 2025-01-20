723d4dfda9927ea9f0c8d4bb6b7905015fa68a8c
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-20 09:39:01 +0500

    medium is-the-word-an-isogram

--- medium/is-the-word-an-isogram/code.js
+++ medium/is-the-word-an-isogram/code.js
@@ -1,5 +1,18 @@
 function isIsogram(str) {
   // Your code here.
+  let letter = '' // bu yerga takrorlanmagan harflarni saqlab boramiz
+  return str.split('').every((word, index) => { // strni split va every qilib tekshiramiz
+    if (index === 0) {  // agar index = 0 bo'lsa letterga word ni qo'shib qo'yamiz
+      letter += word.toLowerCase() // kichik harf qilib
+      return true // true ni return qilamiz every yana ishini davom ettirishi uchun
+    } else if(!letter.includes(word.toLowerCase())) {
+      // aks holda word letter ni ichidan topilmasa uni ham letterga qo'shamiz
+      letter += word.toLowerCase() // kichik harf qilib
+      return true // true ni return qilamiz every yana ishini davom ettirishi uchun
+    } else { // aks holda false ni return qilamiz va shu yerda ish yakunlanadi
+      return false
+    }
+  }) // agar barchashi true bo'lsa javob ga true qaytadi
 }
 
 module.exports = isIsogram;
