dc86cc4845955d3dc67e80c2015aba99fc343105
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-20 09:40:52 +0500

    medium making-a-box

--- medium/making-a-box/code.js
+++ medium/making-a-box/code.js
@@ -1,5 +1,14 @@
 function makeBox(n) {
   // Your code here.
+  let res = [] // bu yerga natijalarni push qilib boramiz
+  for(let i = 1; i <= n; i++) { // n gacha for qilamiz
+    if(i === 1 || i === n) {  // agar i=1 va i oxirgi element bo'lsa
+      res.push("#".repeat(n)) // # ni n gacha takror qilib qo'shamiz
+    } else {  // aks holda boshiga va oxiriga # qo'yib o'rtasini bo'sh joy qilib
+      res.push(`#${" ".repeat(n-2)}#`) // res ga push qilamiz
+    }
+  }
+  return res // res ni retur qilamiz
 }
 
 module.exports = makeBox;
