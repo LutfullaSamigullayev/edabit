8c16b8c9309e4ce10f1f0228c57e7e9a931b2f73
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-20 09:39:23 +0500

    medium leaderboard-sort

--- medium/leaderboard-sort/code.js
+++ medium/leaderboard-sort/code.js
@@ -1,5 +1,9 @@
 function leaderboards(users) {
   // Your code here.
+  const trueScore = (people => people.score + people.reputation * 2)
+  // bu funksiya orqali trueScore ni hisoblaymiz
+  return users.sort((user, user2) => trueScore(user2) - trueScore(user))
+  // sort orqali katta Srore ga ega bo'lganni oldinga o'tirib chiqamiz
 }
 
 module.exports = leaderboards;
