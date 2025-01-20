d930fe8391d91815c36fcd72e2431ca26f7b6fd7
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-20 09:40:30 +0500

    medium magic-date

--- medium/magic-date/code.js
+++ medium/magic-date/code.js
@@ -1,5 +1,13 @@
 function Magic(str) {
   // Your code here.
+  const arr = str.split(' ') // str ni  ' ' joy orqali bo'laklarga bo'lib olamiz
+  const summ = arr[0] * arr[1]  // keyin arr ning kun va oy ni ko'payriamiz
+  if(arr[2] % 10 === summ || arr[2] % 100 === summ || arr[2] % 1000 === summ ) {
+    // agar ko'paytma yilning songi 1, 2 yoki 3 raqamiga teng bo'lsa
+    return true // magic date ga mos keladi va true qaytaramiz
+  } else {
+    return false // aks holda false qaytaramiz
+  }
 }
 
 module.exports = Magic;
