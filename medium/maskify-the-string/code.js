20a32adca8216c820ca24677f3b698e11cf26dc8
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-21 13:16:06 +0500

    medium maskify-the-string

--- medium/maskify-the-string/code.js
+++ medium/maskify-the-string/code.js
@@ -1,5 +1,13 @@
 function maskify(str) {
   // Your code here.
+  let res = '' // bu yerga javobni saqlaymiz
+  if(str.length > 4) {  // agar str ning uzunligi 4 dan katta bo'lsa
+    res = "#".repeat(str.length-4)+str.slice(-4)
+    // songi 4 elementingi olib qolgan joyiga # qo'yib res ga joylaymiz
+  } else { // aks holda str ni res ga joylaymiz
+    res = str
+  }
+  return res // res ni  return qilamiz
 }
 
 module.exports = maskify;
