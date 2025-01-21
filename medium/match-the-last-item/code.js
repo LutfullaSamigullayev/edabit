12c8303014d350a100f5a92c54a2f977043bfe39
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2025-01-21 13:17:01 +0500

    medium match-the-last-item

--- medium/match-the-last-item/code.js
+++ medium/match-the-last-item/code.js
@@ -1,5 +1,12 @@
 function matchLastItem(arr) {
   // Your code here.
+  let res = '' // bu yerda oxirgi elementdan boshqa barcha elementlarni qo'shib boramiz
+  arr.forEach((word, index) => {  // arr ni forEach qilib chiqamiz
+    if(!(arr.length-1 === index)) { // agar oxirgi element bo'lmasa 
+      res += word // res ga wordni qo'shamiz
+    } 
+  })// agar res arr ni oxirgi elementiga mos bo'lsa true aks holda false qaytaradi
+  return arr[arr.length-1] === res 
 }
 
 module.exports = matchLastItem;
