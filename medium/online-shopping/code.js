0788de310e6671aef71c24463ff2f69b4fc5d787
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-23 00:36:07 +0300

    medium online-shopping

--- medium/online-shopping/code.js
+++ medium/online-shopping/code.js
@@ -1,5 +1,10 @@
 function freeShipping(order) {
   // Your code here.
+  const arr = Object.values(order); 
+  // Bu funksiya obyektning faqat qiymatlarini massiv sifatida qaytaradi.
+  const sum = arr.reduce((acc, curr) => acc + curr, 0);
+  // reduce orqali massivni barcha elementlarini qo'shib chiqamiz.
+  return sum > 50; // Agar sum 50 dan katta bo'lsa true qaytaradi.
 }
 
 module.exports = freeShipping;
