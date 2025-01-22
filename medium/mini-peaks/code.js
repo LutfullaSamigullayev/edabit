8346cfb91ad5d1aa673e208867d4658fbe76bdd6
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-22 23:19:18 +0300

    medium mini-peaks

--- medium/mini-peaks/code.js
+++ medium/mini-peaks/code.js
@@ -1,5 +1,12 @@
 function miniPeaks(arr) {
   // Your code here.
+  return arr.filter((num, index, arr) => (
+    index != 0 && index != arr.length-1 && num > arr[index - 1] && num > arr[index + 1]
+    // arr ni filter qilib shartlarga mos kelganlarni return qilamiz
+    // 1- va oxirgi elementlarni return qilmaymiz
+    // num o'zidan oldingi va keyingi elementlardan katta bo'lishi kerak
+    // shu shartlarni bajargan num larni return qilamiz
+  ))
 }
 
 module.exports = miniPeaks;
