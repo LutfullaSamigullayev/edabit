162d4a741a36929b8f8fe2454bc2a883d9aa6855
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-23 15:16:59 +0300

    medium perfect-number

--- medium/perfect-number/code.js
+++ medium/perfect-number/code.js
@@ -1,5 +1,13 @@
 function checkPerfect(num) {
   // Your code here.
+  let sum = 0; // bu yerda javoblarni qo'shib boramiz
+  for (let i = 1; i <= num / 2; i++) {  // bu yerda numning yarim qismiga qarab chiqamiz
+    if (num % i === 0) {  // agar num i ga bo'linadigan bo'lsa
+      sum += i; // sum ga qo'shib boramiz
+    } 
+  } 
+  return sum === num; // agar sum numga teng bo'lsa true qaytaradi
+
 }
 
 module.exports = checkPerfect;
