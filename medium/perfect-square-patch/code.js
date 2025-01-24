fd98c95afa4160ad2089e773388544d3c593e6c7
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-24 15:21:10 +0300

    medium perfect-square-patch

--- medium/perfect-square-patch/code.js
+++ medium/perfect-square-patch/code.js
@@ -1,5 +1,11 @@
 function squarePatch(n) {
   // Your code here.
+  const arr = []; // javobni saqlash uchun array ochamiz
+  for (let i = 0; i < n; i++) { // n marta for qilamiz
+    arr.push(Array(n).fill(n)); 
+    // har bir for qilishda n ta elementdan iborat array qo'shamiz
+  }
+  return arr; // arr ni return qilamiz
 }
 
 module.exports = squarePatch;
