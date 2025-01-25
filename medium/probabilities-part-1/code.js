b7a5c196c09a042dcca605ab7560af6d8758d4e2
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-25 22:38:39 +0300

    medium probabilities-part-1

--- medium/probabilities-part-1/code.js
+++ medium/probabilities-part-1/code.js
@@ -1,5 +1,14 @@
 function probability(arr, num) {
   // Your code here.
+  let count = 0 // bu yerda numdan katta yoki teng elementlar sonini saqlaymiz
+  arr.forEach(e => e >= num && count ++ ) 
+  // arr ning har bir elementini tekshiramiz agar numdan katta yoki teng bo'lsa count ni oshiramiz
+  let res = count / arr.length * 100 
+  // natijani topish uchun count ni arr ning uzunligiga bo'lib 100 ga ko'paytiramiz
+  if(res - Math.trunc(res)) { // agar res ning kasr qismi bo'lsa
+    res = res.toFixed(1) // kasr qismi 1 ga teng bo'lishi uchun toFixed(1) qo'shamiz
+  }
+  return Number(res) // natijani numberga o'tkazib qaytaramiz
 }
 
 module.exports = probability;
