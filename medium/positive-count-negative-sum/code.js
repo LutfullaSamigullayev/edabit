3949778162c473f0da853fabe91835fe79b8623a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-25 22:38:22 +0300

    medium positive-count-negative-sum

--- medium/positive-count-negative-sum/code.js
+++ medium/positive-count-negative-sum/code.js
@@ -1,5 +1,13 @@
 function countPosSumNeg(arr) {
   // Your code here.
+  if(!arr || !arr.length) {  // agar arr bo'sh yoki yo'qligini tekshiramiz
+    return [] // bo'sh massiv qaytaramiz
+  }
+  let count = 0 // bu yerda musbat elementlar sonini saqlaymiz
+  let summ = 0 // bu yerda manfiy elementlar yig'indisini saqlaymiz
+  arr.forEach(num => num > 0 ? count++ : summ += num)
+  // arr ning har bir elementini tekshiramiz agar num musbat bo'lsa count ni oshamiz aks holda summ ga qo'shamiz
+  return [count, summ] // musbat elementlar soni va manfiy elementlar yig'indisini qaytaramiz
 }
 
 module.exports = countPosSumNeg;
