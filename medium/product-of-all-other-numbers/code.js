21650372431bb54bfed76a1e556411e111302ef4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-26 21:34:23 +0300

    medium product-of-all-other-numbers

--- medium/product-of-all-other-numbers/code.js
+++ medium/product-of-all-other-numbers/code.js
@@ -1,5 +1,14 @@
 function getProducts(arr) {
   // Your code here.
+  
+  return arr.map((_, index, array) => {
+    return array.reduce((acc, num, i) => {
+      if(i !== index) {
+        acc *= num
+      }
+      return acc
+    }, 1)
+  })
 }
 
 module.exports = getProducts;
