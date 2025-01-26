d9fc891b8dc809aa7eb7bf47654e50b9de996cb3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-26 21:44:08 +0300

    medium product-of-all-other-numbers

--- medium/product-of-all-other-numbers/code.js
+++ medium/product-of-all-other-numbers/code.js
@@ -1,5 +1,15 @@
 function getProducts(arr) {
   // Your code here.
+  return arr.map((_, index, array) => { 
+    // map orqali arr ni indexi bo'yicha tekshirib chiqamiz
+    return array.reduce((acc, num, i) => {
+      // reduce orqali array ni hisoblashni boshlaymiz
+      if(i !== index) { // agar index i bilan i bir xil bo'lmasa
+        acc *= num // acc ni num ga ko'paytiramiz
+      } // agar index i bilan i bir xil bo'lsa uni qoldirib ketamiz
+      return acc //acc ni return qilamiz
+    }, 1) // reduce ni boshlang'ich qiymati 1
+  }) // map ni qaytarib beramiz
 }
 
 module.exports = getProducts;
