ac84c041dc7d5a73243581b2e22ed77b166bdfb2
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-26 21:48:35 +0300

    medium quadratic-equation

--- medium/quadratic-equation/code.js
+++ medium/quadratic-equation/code.js
@@ -1,5 +1,10 @@
 function quadraticEquation(a, b, c) {
   // Your code here.
+  const discriminant = b ** 2 - 4 * a * c
+  // diskriminant ni topib olamiz
+  const x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+  // x1 ni topib olamiz
+  return x1 // x1 ni qaytarib beramiz
 }
 
 module.exports = quadraticEquation;
