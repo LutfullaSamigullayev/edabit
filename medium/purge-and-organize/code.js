81d92a962624e45affb53c07edb76dfb179d011f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-26 21:46:07 +0300

    medium purge-and-organize

--- medium/purge-and-organize/code.js
+++ medium/purge-and-organize/code.js
@@ -1,5 +1,9 @@
 function uniqueSort(arr) {
   // Your code here.
+  const res = [] // javoblarni saqlash uchun bo'sh array yaratamiz
+  arr.forEach(num => !res.includes(num) && res.push(num))
+  // arr ni forEach orqali tekshiramiz agar res da num yo'q bo'lsa unga qo'shamiz
+  return res.sort((a, b) => a - b) // va res ni sort qilib return qilamiz
 }
 
 module.exports = uniqueSort;
