a06ddfb216461d28eeef7c7158bda4152cc9482a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-27 14:29:10 +0500

    medium ranged-reversal

--- medium/ranged-reversal/code.js
+++ medium/ranged-reversal/code.js
@@ -1,5 +1,13 @@
 function rangedReversal(arr, start, end) {
   // Your code here.
+  const arrStart = arr.slice(0, start); 
+  // bu yerda arr ning 0 dan start gacha bo'lgan elementlarini olamiz
+  const arrCenter = arr.slice(start, end + 1).reverse()
+  // bu yerda arr ning start dan end gacha bo'lgan elementlarini olamiz va ularni teskari tartibda qaytarib beramiz
+  const arrEnd = arr. slice(end + 1)
+  // bu yerda arr ning end dan keyin gacha bo'lgan elementlarini olamiz
+  return [...arrStart, ...arrCenter, ...arrEnd] 
+  // bu yerda olgan elementlarni bir qatorga joylaymiz va reurn qilamiz
 }
 
 module.exports = rangedReversal;
