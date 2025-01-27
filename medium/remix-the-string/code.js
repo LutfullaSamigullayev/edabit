d59e0f621d0b3b29270cb04a8f9229377e8433f4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-27 14:34:00 +0500

    medium remix-the-string

--- medium/remix-the-string/code.js
+++ medium/remix-the-string/code.js
@@ -1,5 +1,12 @@
 function remix(str, arr) {
   // Your code here.
+  let res = [] // javoblarni saqlash uchun bo'sh array yaratamiz
+  str.split('').forEach((word, index) => { 
+    // str ni split orqali harflarga ajratib, har birini forEach orqali tekshiramiz
+    res[arr[index]] = word 
+    //arr ning indexidan kelgan raqamni res indexi sifatida qo'shamiz va harfini joylashtiramiz
+  })
+  return res.join('') // javoblarni join orqali stringga aylantiramiz va return qilamiz
 }
 
 module.exports = remix;
