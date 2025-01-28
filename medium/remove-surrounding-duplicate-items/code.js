e4d0d06bee62e396c2d79f3c02529fc19c986f42
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-28 14:23:32 +0500

    medium remove-surrounding-duplicate-items

--- medium/remove-surrounding-duplicate-items/code.js
+++ medium/remove-surrounding-duplicate-items/code.js
@@ -1,5 +1,14 @@
 function uniqueInOrder(sequence) {
   // Your code here.
+  const res = [] // bu yerga javoblarni saqlab boramiz
+  for(let i = 0; i < sequence.length; i++) {
+    // sequence nign uzunligi bo'yicha for qilib chiqamiz
+    if(res[res.length - 1] !== sequence[i]) { 
+      // agar kelgan element res ning so'ngi elementiga teng bo'lmasa
+      res.push(sequence[i]) // uni res ga push qilamiz
+    }
+  }
+  return res // res ni return qilamiz
 }
 
 module.exports = uniqueInOrder;
