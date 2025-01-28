96ce48951c26faedacee37d4382b77753274dbcc
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-28 14:10:50 +0500

    medium remove-duplicates-from-an-array

--- medium/remove-duplicates-from-an-array/code.js
+++ medium/remove-duplicates-from-an-array/code.js
@@ -1,5 +1,10 @@
 function removeDups(arr) {
   // Your code here.
+  const res = [] // javoblarni saqlab borish uchun bosh array ochamiz
+  arr.forEach(e => !res.includes(e) && res.push(e))
+  // arr ni forEach qilib jar bir elemntni tekshirib chiqamiz
+  // agar element res ichidan topilmasa uni res ga qo'shamiz
+  return res // res ni return qilamiz
 }
 
 module.exports = removeDups;
