8489b2fc503d51f226c8ae4e8a069f16def70023
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-29 21:27:56 +0500

    medium replacing-letters-with-hashes

--- medium/replacing-letters-with-hashes/code.js
+++ medium/replacing-letters-with-hashes/code.js
@@ -1,5 +1,9 @@
 function replace(str, r) {
   // Your code here.
+  const reg = `[${r}]` // r ni [] qavs ichiga joylashtirish
+  const regex = new RegExp(reg, 'g') // reg ni global qilib regex ga aylantirish
+  const result = str.replace(regex, "#") // regex ni # bilan almashtirish
+  return result //result ni return qilamiz
 }
 
 module.exports = replace;
