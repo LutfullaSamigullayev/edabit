8c2faf883523f8667b779a70679b248ce087f6ed
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-29 21:27:30 +0500

    medium remove-the-special-characters-from-a-string

--- medium/remove-the-special-characters-from-a-string/code.js
+++ medium/remove-the-special-characters-from-a-string/code.js
@@ -1,5 +1,8 @@
 function removeSpecialCharacters(str) {
   // Your code here.
-}
+  const regex = /[!@#$%^&\\\*\(\)\[\]{}<>\+=,`~|\.\?]/g;
+  // bu regexda simvollarni joylashtiramiz
+  return str.replace(regex, ''); // replece orqali simvollarni o'chiramiz
+} // va return qilib qaytarib beramiz
 
 module.exports = removeSpecialCharacters;
