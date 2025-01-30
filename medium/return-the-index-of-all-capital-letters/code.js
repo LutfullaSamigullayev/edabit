98cbfb57b9812533267230444d86b608bd18665f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-30 11:00:12 +0500

    medium return-the-index-of-all-capital-letters

--- medium/return-the-index-of-all-capital-letters/code.js
+++ medium/return-the-index-of-all-capital-letters/code.js
@@ -1,5 +1,14 @@
 function indexOfCaps(str) {
   // Your code here.
+  const arr = str.split(''); // str ni split qilib arrayga aylantiramiz
+  const res = [] // javobni saqlash uchun bo'sh array yaratamiz
+  arr.forEach((e, i) => { // arrayni forEach orqali aylantiramiz
+    if(e === e.toUpperCase() && e !== e.toLowerCase()) {
+      // agar element katta harf bo'lsa va kichik harf bo'lmasa
+      res.push(i) // shu elementning indexini res ga qo'shamiz
+    }
+  })
+  return res // res ni return qilamiz
 }
 
 module.exports = indexOfCaps;
