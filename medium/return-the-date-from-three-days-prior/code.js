4ee29df465240c585907429a5940250dbba228bd
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-30 10:59:58 +0500

    medium return-the-date-from-three-days-prior

--- medium/return-the-date-from-three-days-prior/code.js
+++ medium/return-the-date-from-three-days-prior/code.js
@@ -1,5 +1,13 @@
 function threeDaysAgo(date) {
   // Your code here.
+  let newDate = date // date ni newDate ga o'zlashtiramiz
+  newDate.setDate(newDate.getDate() - 3) // newDate ni 3 kun oldinga o'zlashtiramiz
+  const year = newDate.getFullYear() // yilni olishimiz mumkin
+  const month = newDate.getMonth() + 1 // oy ni olishimiz mumkin, lekin 0 dan boshlanadi, shuning uchun +1 qo'shamiz
+  const month2 = month < 10 ? `0${month}` : month // agar oy 10 dan kichik bo'lsa oldingiga 0 qo'shamiz
+  const day = newDate.getDate()  // kuni olishimiz mumkin
+  const day2 = day < 10 ? `0${day}` : day // agar kun 10 dan kichik bo'lsa oldingiga 0 qo'shamiz
+  return `${year}-${month2}-${day2}`// natijani shunday formatda qaytarib beramiz
 }
 
 module.exports = threeDaysAgo;
