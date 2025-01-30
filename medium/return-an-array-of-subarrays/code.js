efec3c9423f937de05d770994ce2f6392c35123d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-30 10:59:46 +0500

    medium return-an-array-of-subarrays

--- medium/return-an-array-of-subarrays/code.js
+++ medium/return-an-array-of-subarrays/code.js
@@ -1,5 +1,9 @@
 function matrix(x, y, z) {
   // Your code here.
+  return Array(x).fill(Array(y).fill(z))
+  // Array(x) -> x ta elementlari bo'lgan array yaratamiz
+  // fill -> arrayni ichiga qanday elementlar qo'shishni bildiradi
+  // Array(y).fill(z) -> y ta elementlari bo'lgan array yaratamiz va ularni z bilan to'ldiramiz
 }
 
 module.exports = matrix;
