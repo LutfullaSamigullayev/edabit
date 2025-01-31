bdd46dff8bb62f78db620e9568702ccc2e3c55dc
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-31 15:48:22 +0500

    medium return-the-middle-character-s-of-a-string

--- medium/return-the-middle-character-s-of-a-string/code.js
+++ medium/return-the-middle-character-s-of-a-string/code.js
@@ -1,5 +1,16 @@
 function getMiddle(str) {
   // Your code here.
+  const leng = str.length // str ning uzunligini olamiz
+  if(leng === 1) { // agar uzunlik 1 bo'lsa
+    return str // str ni o'zini return qilamiz 
+  } 
+  const leng2 = leng/2 // uzunligini 2 ga bo'lib o'rtasini topamiz
+  if(leng % 2 === 0) { // agar uzunlik juft son bo'lsa
+    return str.slice(leng2 - 1, leng2 + 1) 
+    // o'rtadagi 2 ta harfni qaytarib beramiz
+  } else { // aks holda
+    return str.slice(leng2, leng2 + 1)
+  } // o'rtadagi 1 ta harfni qaytarib beramiz
 }
 
 module.exports = getMiddle;
