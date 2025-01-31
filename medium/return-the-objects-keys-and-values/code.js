358ae85d6833efa753977ec27e09906faa691925
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-01-31 15:48:58 +0500

    medium return-the-objects-keys-and-values

--- medium/return-the-objects-keys-and-values/code.js
+++ medium/return-the-objects-keys-and-values/code.js
@@ -1,5 +1,8 @@
 function keysAndValues(obj) {
   // Your code here.
+  return [Object.keys(obj), Object.values(obj)]
+  // Object keys orqali obj ning keylarini olib massivga aylantiramiz
+  // Object values orqali obj ning value larini olib massivga aylantiramiz
 }
 
 module.exports = keysAndValues;
