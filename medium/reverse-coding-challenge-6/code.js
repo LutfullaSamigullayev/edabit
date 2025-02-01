8cdc7c3423f6de1076149634a45f8349ae4fb37f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-01 16:22:08 +0500

    medium reverse-coding-challenge-6

--- medium/reverse-coding-challenge-6/code.js
+++ medium/reverse-coding-challenge-6/code.js
@@ -1,5 +1,9 @@
 function mysteryFunc(num) {
   // Your code here.
+  let summ = 1 // bu yerda javoblarni ko'paytiramiz
+  num.toString().split('').forEach(n => summ *= Number(n))
+  // num ni stringga aylantiramiz, split orqali har bir raqamni ajratib olib forEach orqali ko'paytiramiz
+  return summ // summ ni return qilamiz
 }
 
 module.exports = mysteryFunc;
