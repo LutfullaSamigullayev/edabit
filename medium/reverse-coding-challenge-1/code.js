7b39381a5e8b415a47d179d7b9de00a72f48ee8d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-01 16:21:52 +0500

    medium reverse-coding-challenge-1

--- medium/reverse-coding-challenge-1/code.js
+++ medium/reverse-coding-challenge-1/code.js
@@ -1,5 +1,12 @@
 function mysteryFunc(str) {
   // Your code here.
+  let res = '' // javobni saqlash uchun bo'sh string
+  for (let i = 0; i < str.length; i += 2) { 
+    // for loop orqali str ni aylanib chiqamiz
+    res += str[i].repeat(str[i + 1])
+    // res ga str[i] ni str[i + 1] marta qo'shib yozamiz
+  }
+  return res // res ni return qilamiz
 }
 
 module.exports = mysteryFunc;
