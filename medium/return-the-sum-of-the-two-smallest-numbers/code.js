88fc1d51337ef312987c4c0653bfd1073a2f49fc
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-01 16:21:37 +0500

    medium return-the-sum-of-the-two-smallest-numbers

--- medium/return-the-sum-of-the-two-smallest-numbers/code.js
+++ medium/return-the-sum-of-the-two-smallest-numbers/code.js
@@ -1,5 +1,9 @@
 function sumTwoSmallestNums(arr) {
   // Your code here.
+  const newarr = arr.filter(num => num >= 0).sort((a, b) => a-b)
+  // arr ni filter qilib manfiy sonlarni olib tashlab chiqamiz
+  // keyingi qadamda sort qilib chiqamiz
+  return newarr[0] + newarr[1] // eng kichik 2 ta sonni qo'shib chiqamiz
 }
 
 module.exports = sumTwoSmallestNums;
