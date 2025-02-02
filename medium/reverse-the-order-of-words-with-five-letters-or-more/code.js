5e1994dcbdc00f5c7d37436e5606515bb920a2fb
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-02 22:57:21 +0500

    medium reverse-the-order-of-words-with-five-letters-or-more

--- medium/reverse-the-order-of-words-with-five-letters-or-more/code.js
+++ medium/reverse-the-order-of-words-with-five-letters-or-more/code.js
@@ -1,5 +1,15 @@
 function reverse(str) {
   // Your code here.
+  const arr = str.split(' ') // str ni split qilib so'zlarga bo'lib olamiz
+  const res = arr.map(word => { // hosil bo'lgan arr ni map qilib chiqamiz
+    if(word.length > 4) { // agar kelgan so'zning uzunligi 4 dan katta bo'lsa
+      return word.split('').reverse().join('')
+      // uni teskari qilib return qilamiz
+    } else { // aks holda
+      return word // so'zni o'zini qaytaramiz
+    }
+  })
+  return res.join(' ') // javoblarni qo'shib string sifatida return qilamiz
 }
 
 module.exports = reverse;
