eac74e0ca82b1471c0e61b3ac878c4117c32e2c6
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-04 11:29:33 +0500

    medium something-in-the-box

--- medium/something-in-the-box/code.js
+++ medium/something-in-the-box/code.js
@@ -1,5 +1,9 @@
 function inBox(arr) {
   // Your code here.
+  return arr.join('').includes('*') 
+  // join orqali arr ni barcha elementlarini birlashtirib string qilamiz
+  // va includes orqali ichidan '*'-yulduzchani qidiramiz
+  // agar topilsa true aks holda false qataradi
 }
 
 module.exports = inBox;
