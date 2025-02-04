995a142bf0d586414ac68dcc7bd4b90cbd1a66c5
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-04 11:28:09 +0500

    medium simon-says

--- medium/simon-says/code.js
+++ medium/simon-says/code.js
@@ -1,5 +1,10 @@
 function simonSays(arr1, arr2) {
   // Your code here.
+  arr1.pop() // arr1 ni pop orqali oxirgi elementini olib tashlaymiz
+  arr2.shift() // arr2 ni shift orqali birinchi elementini olib tashlaymiz
+  return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index])
+  // ularning uzligi teng bo'lsa every orqali har birini tekshirib chiqamiz
+  // agar hammasi bir xil bo'lsa true qaytaradi aks holda false
 }
 
 module.exports = simonSays;
