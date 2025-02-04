4f6fd7ecc5178ca945fb9a47b620e0a4d123adb9
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-04 11:28:42 +0500

    medium smallest-n-digit-number

--- medium/smallest-n-digit-number/code.js
+++ medium/smallest-n-digit-number/code.js
@@ -1,5 +1,11 @@
 function smallest(digits, value) {
   // Your code here.
+  // Eng kichik N xonali sonni aniqlash
+  let start = Math.pow(10, digits - 1);
+  // start dan katta yoki teng bo'lib, num ga bo'linadigan eng kichik sonni topish
+  let result = Math.ceil(start / value) * value;
+  return result;
 }
 
+console.log(smallest(5, 12));
 module.exports = smallest;
