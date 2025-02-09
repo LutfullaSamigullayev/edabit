d985305efcf5cee637b754d25540685a6b94558e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-09 12:47:23 +0500

    medium sort-by-index-of-character

--- medium/sort-by-index-of-character/code.js
+++ medium/sort-by-index-of-character/code.js
@@ -1,5 +1,9 @@
 function sortByCharacter(arr, n) {
   // Your code here.
+  return arr.sort((a, b) => a[n-1].localeCompare(b[n-1]))
+  // localeCompare - ikki belgini alfavit tartibida solishtiradi
+  // har bir elementning n-chi belgisi bo'yicha 
+  // 0-indeksdan boshlanganligi uchun n dan 1 ni ayiramiz
 }
 
 module.exports = sortByCharacter;
