01287ac8699e152c69e961ba08311518ef652149
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-09 12:22:26 +0500

    medium sort-a-string-by-its-last-character

--- medium/sort-a-string-by-its-last-character/code.js
+++ medium/sort-a-string-by-its-last-character/code.js
@@ -1,5 +1,13 @@
 function sortByLast(str) {
   // Your code here.
+  const arr = str.split(' ') // matnni so'zlarga bo'lamiz
+  const res = arr.sort((a, b) => {
+    let lastA = a[a.length - 1]; // a ning oxirgi harfi
+    let lastB = b[b.length - 1]; // b ning oxirgi harfi
+    return lastA.localeCompare(lastB); 
+    // localeCompare harflarni alfavit bo‘yicha solishtiradi
+  }); // hosil bo'lgan arrayni string ga aylantiramiz va return qilamiz
+  return res.join(' ') 
 }
 
 module.exports = sortByLast;
