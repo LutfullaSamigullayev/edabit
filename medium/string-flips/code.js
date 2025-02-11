8d0af4c82fcf4ae97869936e7e9604391cc69558
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-11 23:30:52 +0500

    medium string-flips

--- medium/string-flips/code.js
+++ medium/string-flips/code.js
@@ -1,5 +1,16 @@
 function flip(str, spec) {
   // Your code here.
+  if(spec === 'sentence') { // agar spec "sentence" ga teng bo'lsa
+    return str.split(' ').reverse().join(' ')
+    // str ni so'zlarga bo'lib hosil bo'lgan massivni teskarisiga aylantirib
+    // join orqali birlashtirib string qilib return qilamiz
+  } else { // aks holda spec "word" ga teng bo'lsa 
+    return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+    // str ni so'zlarga bo'lib map qilib har bir so'zni harflarga bo'lib 
+    // harflardan hosil bo'lgan massivni teskarisiga aylantirib 
+    // yana birlashtirib teskari so'zlardan hosil bo'lgan array chiqadi
+    // array ni ham join orqali birlashtirib string sifatida return qilamiz
+  }
 }
 
 module.exports = flip;
--- medium/string-flips/code.spec.js
+++ medium/string-flips/code.spec.js
@@ -1,14 +1,18 @@
-const flip = require('./code');
+const flip = require('./code'); // Asosiy kodni import qilish
 
 describe('Tests', () => {
     test('the tests', () => {
+        const str1 = "There's never enough time to do all the nothing you want";
+        const str2 = "I have all these great genes but they're recessive";
+        const str3 = "I like maxims that don't encourage behavior modification";
+
         expect(flip('Hello', 'word')).toEqual('olleH');
         expect(flip('Hello', 'sentence')).toEqual('Hello');
-        expect(flip(str1, 'word')).toEqual('s\'erehT reven hguone emit ot od lla eht gnihton uoy tnaw');
-        expect(flip(str1, 'sentence')).toEqual('want you nothing the all do to time enough never There\'s');
-        expect(flip(str2, 'word')).toEqual('I evah lla eseht taerg seneg tub er\'yeht evissecer');
-        expect(flip(str2, 'sentence')).toEqual('recessive they\'re but genes great these all have I');
-        expect(flip(str3, 'word')).toEqual('I ekil smixam taht t\'nod egaruocne roivaheb noitacifidom');
-        expect(flip(str3, 'sentence')).toEqual('modification behavior encourage don\'t that maxims like I');
+        expect(flip(str1, 'word')).toEqual("s'erehT reven hguone emit ot od lla eht gnihton uoy tnaw");
+        expect(flip(str1, 'sentence')).toEqual("want you nothing the all do to time enough never There's");
+        expect(flip(str2, 'word')).toEqual("I evah lla eseht taerg seneg tub er'yeht evissecer");
+        expect(flip(str2, 'sentence')).toEqual("recessive they're but genes great these all have I");
+        expect(flip(str3, 'word')).toEqual("I ekil smixam taht t'nod egaruocne roivaheb noitacifidom");
+        expect(flip(str3, 'sentence')).toEqual("modification behavior encourage don't that maxims like I");
     });
 });
