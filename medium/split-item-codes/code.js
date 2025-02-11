4cf56d8a3f8f2d60ee1403a8dea91ad964b97cfb
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-11 12:08:45 +0500

    medium split-item-codes

--- medium/split-item-codes/code.js
+++ medium/split-item-codes/code.js
@@ -1,5 +1,21 @@
 function splitCode(item) {
   // Your code here.
+  let str = '' // bu yerga stringlarni saqlaymiz
+  let num = '' // bu yerga numberlarni saqlaymiz
+  for(let word of item) { 
+    // item ni for qilamiz of orqali uning elementlarini wordga o'zlashtiramiz
+    if(String(Number(word)) === "NaN"){ 
+      // wordni number ga aylantiramiz va javobni string qilamiz,
+      // agar word da string bo'lsa u raqamga aylanmaydi va NaN qaytaradi,
+      // biz NaN === NaN deb tekshirib bilmaymiz u bizga false qaytaradi,
+      // shuning uchun javobni string ga aylantiramiz agar u string bo'lsa 'NaN' qaytadi,
+      // aks holda number bo'lsa o'sha raqam string ko'rinishida qaytadi: '5'.
+      str += word // "NaN" === "NaN" true bo'lsa u string, uni str ga qo'shamiz
+    } else { // aks ho'lda u number bo'lsa ozini holatida yani string ko'rinishida num ga qo'shamiz
+      num += word // agar uni number shaklida qo'shsak u matematik amal bajarib yuboradi
+    } 
+  } // str ni string ko'rinishida, num ni + orqali Number ga aylantirib array shaklida return qilamiz
+  return [str, +num]
 }
 
 module.exports = splitCode;
