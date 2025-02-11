a05b50a654663ef5835d42d71c1ac85a6f5f4eae
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-11 23:31:15 +0500

    medium subset-validation

--- medium/subset-validation/code.js
+++ medium/subset-validation/code.js
@@ -1,5 +1,9 @@
 function validateSubsets(subsets, set) {
   // Your code here.
+  return subsets.every(arr => arr.every(num => set.includes(num)))
+  // subsets ni every orqali ichida barcha array ni tekshirib chiqamiz,
+  // arr ni ichidagi raqamlarni set ichidan qidiramiz
+  // agar barchasi topilsa true qaytaradi aks holda false 
 }
 
 module.exports = validateSubsets;
