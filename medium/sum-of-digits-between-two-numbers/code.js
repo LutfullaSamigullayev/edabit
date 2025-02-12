162d9bba24d8514f93d3475b66f9e78349e1d1c9
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-12 21:37:27 +0500

    medium sum-of-digits-between-two-numbers

--- medium/sum-of-digits-between-two-numbers/code.js
+++ medium/sum-of-digits-between-two-numbers/code.js
@@ -1,5 +1,12 @@
 function sumDigits(a, b) {
   // Your code here.
+  let numbers = '' 
+  // bu yerga raqamlar ketma-ketligini string ko'rinishida saqlaymiz
+  for(let i = a; i <= b; i++) { // a dan b gacha for qilib chiqamiz
+    numbers += i // va har bir kelgan raqamni numbers ga saqlab boramiz
+  } // va split qilib arrayni reduce orqali qo'shib chiqamiz
+  return numbers.split('').reduce((a, b) => a + Number(b), 0)
+  // reduce barcha raqamlarni qo'shib number qaytaradi
 }
 
 module.exports = sumDigits;
