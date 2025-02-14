6fa565317d80e1226acb01ff4b1c53f91795ad27
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-14 16:03:02 +0500

    medium tidy-title-and-author-strings

--- medium/tidy-title-and-author-strings/code.js
+++ medium/tidy-title-and-author-strings/code.js
@@ -1,5 +1,13 @@
 function tidyBooks(arr) {
   // Your code here.
+  const res = [] // bu yerga javoblarni push qilib boramiz
+  arr.forEach(str => str.split('-').forEach(word => res.push(word.trim())))
+  // arr ni forEach orqali har bir kitobni olib chiqamiz
+  // va uni split orqali '-'belgi bo'yicha bo'laklarga bo'lib
+  // hosil bo'lgan str[array] ni forEach qilib
+  // har bir so'zni trim bilan oldi va oxiridagi ortiqcha bo'sh joylarni olib
+  // res ga push qilib chiqamiz
+  return res // res ni return qilamiz
 }
 
 module.exports = tidyBooks;
