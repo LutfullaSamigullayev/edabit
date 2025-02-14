759ae0189038dcf989736d837daa2c7b52afee43
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-14 16:01:20 +0500

    medium the-museum-of-incredibly-dull-things

--- medium/the-museum-of-incredibly-dull-things/code.js
+++ medium/the-museum-of-incredibly-dull-things/code.js
@@ -1,5 +1,12 @@
 function removeSmallest(arr) {
   // Your code here.
+  const min = Math.min(...arr) 
+  // Math.min orqali arr ichidagi eng kichigini topamiz
+  const minIndex = arr.findIndex(num => num === min)
+  // findIndex orqali birinchi duch kelgan eng kichik raqamni indexini qaytaramiz
+  arr.splice(minIndex, 1) 
+  // va slpice orqali o'sha indexda joylashgan elementni ochirib arr ni o'zgartiramiz
+  return arr // o'zgargan arrayni return qilamiz
 }
 
 module.exports = removeSmallest;
