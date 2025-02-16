6eeb5f5290580bee437f677d17deb7c6620a0a5c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-16 12:48:24 +0500

    medium truncate-string-at-a-given-length

--- medium/truncate-string-at-a-given-length/code.js
+++ medium/truncate-string-at-a-given-length/code.js
@@ -1,5 +1,11 @@
 function truncate(string, length) {
   // Your code here.
+  const newStr = string.slice(0,length).split(' ') 
+  // oldin berilgan uzunlik bo'yicha satrni qirqib olib uni so'zlarga bo'lib qo'yamiz
+  const arr = string.split(' ') // string ni ham so'zlarga bo'lib qo'yamiz
+  const res = newStr.filter(word => arr.includes(word)) 
+  // bo'lingan satrni ichida so'zlarni to'liqlarini asosoiy satrdan qarab filtrlab o'tiramiz
+  return res.join(' ') // va javobni birlashtirib satr qilib return qilamiz
 }
 
 module.exports = truncate;
