3916c66fc1c3bb268f1b96cb99af32c008ca76b3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-16 13:22:58 +0500

    medium uno-part-1

--- medium/uno-part-1/code.js
+++ medium/uno-part-1/code.js
@@ -1,5 +1,10 @@
 function canPlay(hand, face) {
   // Your code here.
+  const arr = hand.join(' ').split(' ') 
+  // hand ni bo'sh joylar bilan qo'shib yana bo'sh joylar bilan bo'laklarga aylantiramiz
+  const res = face.split(' ').filter(e => arr.includes(e))
+  // face ni bo'sh jo'ylar orqali bo'laklarga bo'lib hand ni ichida borlarini filterlab o'tiramiz
+  return res.length ? true : false // agar res ning uzunligi bo'lsa true aks holda false qaytaramiz
 }
 
 module.exports = canPlay;
