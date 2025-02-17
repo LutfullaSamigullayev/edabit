f7a17701bf8441c5a7f5bce6562a54b27352befe
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-17 21:58:28 +0500

    medium volume-of-a-cone

--- medium/volume-of-a-cone/code.js
+++ medium/volume-of-a-cone/code.js
@@ -1,5 +1,11 @@
 function coneVolume(h, r) {
   // Your code here.
+  // Konus hajmi formulasi: (1/3) * π * r**2 * h
+  return Number(((1/3) * Math.PI * r**2 * h).toFixed(2))
+  // (1/3 * Math.PI * r**2 * h) - konus hajmini hisoblash
+  // toFixed(2) - natijani 2 xonali kasr qismigacha yaxlitlaydi 
+  // va string ko'rinishida return qiladi 
+  // Number() orqali javobni Number tipiga o'tkazamiz
 }
 
 module.exports = coneVolume;
