88702ef410fe6e9a71711af323e0a3a351ed23c8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-17 21:58:08 +0500

    medium valid-hex-code

--- medium/valid-hex-code/code.js
+++ medium/valid-hex-code/code.js
@@ -1,5 +1,10 @@
 function isValidHexCode(str) {
   // Your code here.
+  return /^#[A-Fa-f0-9]{6}$/.test(str);
+  // ^ - Stringning boshidan tekshirishni boshlaydi
+  // # - String "#" belgisi bilan boshlanishi kerak
+  // [A-Fa-f0-9]{6} - Keyingi 6 ta belgi "A-F", "a-f" yoki "0-9" bo'lishi kerak
+  // $ - String oxirigacha faqat shu belgilar bo'lishi kerak
 }
 
 module.exports = isValidHexCode;
