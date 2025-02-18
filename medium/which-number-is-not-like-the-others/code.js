ba2ddea164d9cfa6ca41a683f6f617a346237268
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-18 21:37:59 +0500

    medium which-number-is-not-like-the-others

--- medium/which-number-is-not-like-the-others/code.js
+++ medium/which-number-is-not-like-the-others/code.js
@@ -1,5 +1,20 @@
 function unique(arr) {
   // Your code here.
+  let count = new Map();
+  // Har bir sonni sanash uchun Map yaratamiz
+
+  // 1. Har bir sonni sanash
+  for (let num of arr) {
+    count.set(num, (count.get(num) || 0) + 1);
+    // Agar son Mapda mavjud bo'lsa, qiymatini 1 ga oshiramiz
+    // Agar son Mapda mavjud bo'lmasa, qiymatini 1 ga tenglashtiramiz
+  }
+
+  // 2. Faqat 1 marta uchragan sonni topish
+  for (let [key, value] of count) {
+    if (value === 1) return key;
+    // Agar sonning qiymati 1 ga teng bo'lsa, o'sha sonni qaytarish
+  }
 }
 
 module.exports = unique;
