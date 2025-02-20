11b6f3931e103ec5ddcc307082b5bef634c19d85
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-20 11:25:50 +0500

    hard 3d-visual-block-sequence

--- hard/3d-visual-block-sequence/code.js
+++ hard/3d-visual-block-sequence/code.js
@@ -1,5 +1,8 @@
 function blocks(step) {
   // Your code here.
+  if (step === 0) return 0; // Agar step 0 bo'lsa, bloklar yo'q
+  // Bloklar formulasini 
+  return Math.round(0.5 * step * step + 5.5 * step - 1);
 }
 
 module.exports = blocks;
