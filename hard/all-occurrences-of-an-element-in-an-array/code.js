6cc9c0415c13b8c406c3f891996e5779f352e0d7
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-21 09:59:31 +0500

    hard all-occurrences-of-an-element-in-an-array

--- hard/all-occurrences-of-an-element-in-an-array/code.js
+++ hard/all-occurrences-of-an-element-in-an-array/code.js
@@ -1,5 +1,12 @@
 function getIndices(arr, el) {
   // Your code here.
+  const res = [] // indexlarni saqlash uchun bo'sh massiv
+  arr.forEach((e,i) => { // arr ni forEach qilib tekshirib chiqamiz
+    if(e === el) { // agar e el ga teng bo'lsa
+      res.push(i) // uning indexini res ga push qilamiz
+    }
+  })
+  return res // res ni return qilamiz
 }
 
 module.exports = getIndices;
