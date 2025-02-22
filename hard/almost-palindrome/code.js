710c1ae8b36144aea0026d1379c39da653ffbce4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-22 07:54:48 +0500

    hard almost-palindrome

--- hard/almost-palindrome/code.js
+++ hard/almost-palindrome/code.js
@@ -1,5 +1,12 @@
 function almostPalindrome(str) {
   // Your code here.
+  const polindrom = str.split('').reverse() 
+  // str ni teskarisini polindromga joylaymiz
+  const summ = polindrom.filter((word, index) => word !== str[index])
+  // polindrom ni filter qilib str bilan solishtirib chiqamiz
+  // joylashuv bo'yicha bir xil bo'lmagan harflarni return qilamiz
+  return summ.length === 2 
+  // agar summ ni length 2 ga teng bo'lsa true aks holda false return qilamiz
 }
 
 module.exports = almostPalindrome;
