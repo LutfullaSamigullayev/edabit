87691b6fa4dfb19d9c0ec2c7e6f922f8f5a46614
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-23 12:07:41 +0500

    hard anonymous-name

--- hard/anonymous-name/code.js
+++ hard/anonymous-name/code.js
@@ -1,5 +1,13 @@
 function isCorrectAliases(names, aliases) {
   // Your code here.
+  return aliases.every((str, index) => { 
+    // aliases ni every qilib har bir ism va familyani tekshirib chiqamiz
+    return str.split(' ').every(word => { 
+      // va ism va familyalarni bo'laklarga bo'lib har birini tekshirib chiqamiz
+      return word[0] === names[index][0] 
+      // ism va familyalarning 1-harfi names ning shu o'rinda joylashgan ism ning 1-harfi bilan mos kelishini tekshiramiz
+    }) // agar barchasi mos kelsa true aks holda false ni return qilamiz
+  })
 }
 
 module.exports = isCorrectAliases;
