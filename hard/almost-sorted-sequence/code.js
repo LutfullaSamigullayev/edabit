aea668a1390fc8f0aac2bf6a0c9e61b420a6cd9f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-23 12:07:12 +0500

    hard almost-sorted-sequence

--- hard/almost-sorted-sequence/code.js
+++ hard/almost-sorted-sequence/code.js
@@ -1,5 +1,18 @@
 function almostSorted(arr) {
   // Your code here.
+  let decreasing = 0 // kamayuvchilarni hisoblab boramiz
+  let increasing = 0 // ko'payuvchilarni hisoblab boramiz
+  arr.forEach((num, i, arr) => { 
+    // arr ni forEach qilib o'zidan oldingi element bilan solishtirib chiqamiz
+    if (num > arr[i + 1]) { 
+      // agar num o'zidan oldingi elementdan katta bo'lsa
+      decreasing++ // decreasing ni 1 ga oshiramiz
+    } else if (num < arr[i + 1]) {
+       // agar num o'zidan oldingi elementdan kichik bo'lsa
+      increasing++ // increasing ni 1 ga oshiramiz
+    }
+  }) // decreasing yoki increasing 1 ga teng bo'lsa true qaytaramiz
+  return decreasing === 1 || increasing === 1 
 }
 
 module.exports = almostSorted;
