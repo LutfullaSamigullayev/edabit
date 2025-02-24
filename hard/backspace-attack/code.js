17a611457d810b130f25758fd16252e118a8122b
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-24 16:19:26 +0500

    hard backspace-attack

--- hard/backspace-attack/code.js
+++ hard/backspace-attack/code.js
@@ -1,5 +1,14 @@
 function erase(s) {
   // Your code here.
+  const res = [] // bu yerga hafrlarni qo'shib boramiz
+  s.split('').forEach(word => { // s ni bo'lakalrga bo'lib har birini tekshirib chiqamiz
+    if(word === '#') { // agar word # ga teng bo'lsa 
+      res.pop() // res dan so'ngi belgini o'chiramiz
+    } else { // aks holda
+      res.push(word) // uni res ga qo'shamiz
+    }
+  })
+  return res.join('') // res ni stringga aylantirib return qilamiz
 }
 
 module.exports = erase;
