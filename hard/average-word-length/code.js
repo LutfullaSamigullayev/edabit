55c1416e669ebe41e454132f3e338f4302e24e95
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-24 16:19:08 +0500

    hard average-word-length

--- hard/average-word-length/code.js
+++ hard/average-word-length/code.js
@@ -1,5 +1,12 @@
 function averageWordLength(str) {
   // Your code here.
+  let res = 0 // bu yerga so'zlarning uzunligini qo'shib boramiz
+  let words = str.replace(/[^\w\s]/g, "") // str dan tinish belgilarini olib tashlaymiz
+  let arr = words.split(' ') // keyin uni so'zlarga bo'lib chiqamiz
+  arr.forEach(word => res+= word.length) // har bir so'zning uzunligini res ga qo'shamiz
+  return Number((res/arr.length).toFixed(2)) // natijani so'zlarning soniga bo'lib, 
+  // 2 ta kasrgacha yaxlitlab, Number ga o'tkazib return qilamiz
 }
 
+console.log(averageWordLength("Dude, this is so awesome!"))
 module.exports = averageWordLength;
