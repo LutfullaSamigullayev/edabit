9989454fe1f59508d1b03b2919e63e1a3bf4e4f9
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-02-26 08:03:55 +0500

    hard bell-number

--- hard/bell-number/code.js
+++ hard/bell-number/code.js
@@ -1,5 +1,21 @@
 function bell(num) {
   // Your code here.
+  if (num === 0) return 1; // Agar x = 0 bo'lsa, natija har doim 1
+
+  let bellTriangle = [[1]]; // Bell uchburchagini boshlash
+
+  for (let i = 1; i <= num; i++) {
+    let row = []; 
+    row[0] = bellTriangle[i - 1][i - 1]; // Har bir satrning birinchi elementi oldingi satrning oxirgi elementi bilan teng
+    
+    for (let j = 1; j <= i; j++) {
+      row[j] = row[j - 1] + bellTriangle[i - 1][j - 1]; // Har bir keyingi element chap tomondagi va oldingi satrdagi ustun yig'indisidan hosil bo'ladi
+    }
+    
+    bellTriangle.push(row); // Yangi satrni uchburchakka qo'shamiz
+  }
+  
+  return bellTriangle[num][0]; // Bell soni har doim uchburchakning chap tomoni bo'ylab joylashgan
 }
 
 module.exports = bell;
