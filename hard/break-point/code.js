e02a67d69b80b1ebf88308287a4bae38f9c5b433
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-03-12 16:53:35 +0500

    hard break-point

--- hard/break-point/code.js
+++ hard/break-point/code.js
@@ -1,5 +1,18 @@
 function breakPoint(num) {
   // Your code here.
+  let start = 0 // bu yerga boshidagi raqamlar yigindisini qo'shib boramiz
+  let end = 0 // bu yerga oxiridagi raqamlar yigindisini qo'shib boramiz
+  let arr = num.toString().split('') // raqamni stringga aylantirib
+  // bo'laklarga bo'lamiz va arr o'zgaruvchiga saqlab qo'yamiz
+  while(arr.length > 0) { // arr bo'shaguncha sikl aylantiramiz
+    if(start === 0 || start < end) { // agar start 0 ga teng bo'lsa 
+      // yoki start end dan kichik bo'lsa
+      start += Number(arr.shift()) // start ga arr ning boshidagi raqamni qo'shamiz
+    } else { // aks holda end start dan kichik bo'lsa
+      end += Number(arr.pop()) // end ga arr ning oxiridagi raqamni qo'shamiz
+    }
+  }
+  return start === end // start va end ning teklikini return qilamiz
 }
 
 module.exports = breakPoint;
