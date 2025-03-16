fe179ad8abdb84498430683c7a216aa207c74120
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-03-16 14:57:52 +0500

    hard bridge-shuffle

--- hard/bridge-shuffle/code.js
+++ hard/bridge-shuffle/code.js
@@ -1,5 +1,18 @@
 function bridgeShuffle(arr1, arr2) {
   // Your code here.
+  const res = [] // bu yerga natijalarni push qilib boramiz
+
+  while(arr1.length > 0 || arr2.length > 0) {
+    // arr1 va arr2 lar bo'shaguncha sikl aylantiramiz
+    if(arr1.length) { // agar arr1 ning uzunligi bo'lsa
+      res.push(arr1.shift()) // undagi birinchi element ni res ga o'tiramiz
+    }
+    if(arr2.length) { // agar arr1 ning uzunligi bo'lsa
+      res.push(arr2.shift()) // undagi birinchi element ni res ga o'tiramiz
+    }
+  }
+
+  return res // res ni return qilamiz
 }
 
 module.exports = bridgeShuffle;
