dc5d351c605720294e9b73ec6e5e357c8530d95a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-03-18 16:43:56 +0500

    hard building-a-staircase

--- hard/building-a-staircase/code.js
+++ hard/building-a-staircase/code.js
@@ -1,5 +1,18 @@
 function buildStaircase(height, block) {
   // Your code here.
+  let staircase = []; // bu yerga javoblarni push qilib boramiz
+    
+    for (let i = 1; i <= height; i++) { // height bo'yicha for qilamiz
+        let row = Array(height).fill("_"); 
+        // Array(heigth) berib shu uzunlik bo'yicha yangi bo'sh array hosil qilamiz
+        // fill orqali ichini "_" - bilan to'ldiramiz
+        row.fill(block, 0, i); 
+        // keyin hosil bo'lagan arrayni fill orqali 0-indexdan boshlab i-indexgacha
+        // kerakli blok bilan to'ldiramiz
+        staircase.push(row); // hosil bo'lgan arrayni push qilamiz
+    }
+    
+    return staircase; // va natijani retun qilamiz
 }
 
 module.exports = buildStaircase;
