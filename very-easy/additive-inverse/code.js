476c034ee0799ef2f98c19f477f3f21fe587462c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-07 08:04:02 +0500

    very-easy additive-inverse

--- very-easy/additive-inverse/code.js
+++ very-easy/additive-inverse/code.js
@@ -1,5 +1,10 @@
 function additiveInverse(arr) {
   // Your code here.
+  return arr.map(num => num > 0 ? num * (-1) : Math.abs(num))
+  // arr ni map qilib har bir kelgan sonni tekshiramiz 
+  // agar u 0 dan katta bo'lsa uni -1 ga ko'paytiramiz
+  // aks holda uni musbat son qilamiz 
+  // va javoblarni massiv ko'rinishida return qilamiz
 }
 
 module.exports = additiveInverse;
