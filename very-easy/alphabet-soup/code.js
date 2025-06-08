d3324483bbaa602011903b876e60f5448911c00c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-08 05:31:38 +0500

    very-easy alphabet-soup

--- very-easy/alphabet-soup/code.js
+++ very-easy/alphabet-soup/code.js
@@ -1,5 +1,9 @@
 function AlphabetSoup(str) {
   // Your code here.
+  return str.split('').sort().join('');
+  // split() metodi satrni bolaklarga bo'lib arrayga aylantiradi
+  // sort() metodi arrayni tartiblab chiqadi
+  // join() metodi arrayni  elementlarini bir qatorga yig'adi va string qilib qaytaradi
 }
 
 module.exports = AlphabetSoup;
