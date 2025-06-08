5a344cab75298f8f913c6b5771cedf8365eb4509
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-08 06:00:25 +0500

    very-easy array-from-comma-delimited-string

--- very-easy/array-from-comma-delimited-string/code.js
+++ very-easy/array-from-comma-delimited-string/code.js
@@ -1,5 +1,10 @@
 function toArray(str) {
   // Your code here.
+  if(str.length === 0) { // Agar str ning uzunligi 0 ga teng bo'lsa
+    return [] // bo'sh massiv ni return qilamiz
+  } 
+  // str ni split(', ')-"vergul va bo'sh joy" orqali bo'laklarga bo'lib array sifatida return qilamiz
+  return str.split(', ')
 }
 
 module.exports = toArray;
