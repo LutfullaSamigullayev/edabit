8895e00374085acf3d453ea5216cfa2fde38d364
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-09 21:44:53 +0500

    very-easy between-words

--- very-easy/between-words/code.js
+++ very-easy/between-words/code.js
@@ -1,5 +1,9 @@
 function isBetween(first, last, word) {
   // Your code here.
+  // Berilgan uchta so‘zni alfavit bo‘yicha tartibga solamiz
+  const res = [first, last, word].sort();
+  // Agar 'word' alfavit bo‘yicha o‘rtada turgan bo‘lsa, true qaytariladi
+  return res[1] === word;
 }
 
 module.exports = isBetween;
