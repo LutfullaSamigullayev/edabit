f7b43c66375ba21ba0b65eef38802c61c7419b03
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-09 21:46:57 +0500

    very-easy capture-the-rook

--- very-easy/capture-the-rook/code.js
+++ very-easy/capture-the-rook/code.js
@@ -1,5 +1,7 @@
 function canCapture([yourRook, opponentsRook]) {
   // Your code here.
+  // Agar birinchi harflar (ustunlar) yoki ikkinchi raqamlar (qatorlar) bir xil bo‘lsa
+  return yourRook[0] === opponentsRook[0] || yourRook[1] === opponentsRook[1];
 }
 
 module.exports = canCapture;
