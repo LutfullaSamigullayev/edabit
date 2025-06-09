690b38e4a2157215433f8904277424bacce8e98e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-09 21:46:17 +0500

    very-easy calculate-determinant-of-a-2x2-matrix

--- very-easy/calculate-determinant-of-a-2x2-matrix/code.js
+++ very-easy/calculate-determinant-of-a-2x2-matrix/code.js
@@ -1,5 +1,10 @@
 function calcDeterminant(matrix) {
   // Your code here.
+  // Matritsaning elementlarini ajratib olish
+  const [[a, b], [c, d]] = matrix;
+  
+  // Determinant formulasi: ad - bc
+  return (a * d) - (b * c);
 }
 
 module.exports = calcDeterminant;
