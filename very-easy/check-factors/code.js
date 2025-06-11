e79c3fd08e7f1a82841b9846abf0d1785eae2790
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-11 14:39:13 +0500

    very-easy check-factors

--- very-easy/check-factors/code.js
+++ very-easy/check-factors/code.js
@@ -1,5 +1,7 @@
 function checkFactors(factors, num) {
   // Your code here.
+  // Har bir elementni tekshiramiz, agar num ga bo‘linmasa, false qaytaramiz
+  return factors.every(factor => num % factor === 0);
 }
 
 module.exports = checkFactors;
