5e6c8e7afdec1ff8b7d70d0a85dc0fc26abcba93
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-11 14:36:50 +0500

    very-easy case-insensitive-comparison

--- very-easy/case-insensitive-comparison/code.js
+++ very-easy/case-insensitive-comparison/code.js
@@ -1,5 +1,7 @@
 function match(s1, s2) {
   // Your code here.
+  // Har ikkala stringni kichik harflarga o'tkazamiz va taqqoslaymiz
+  return s1.toLowerCase() === s2.toLowerCase()
 }
 
 module.exports = match;
