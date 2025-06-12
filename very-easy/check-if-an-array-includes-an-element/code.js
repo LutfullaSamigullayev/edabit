571d0ea084510dc55083646da1221b0f70ff50fc
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-12 08:37:55 +0500

    very-easy check-if-an-array-includes-an-element

--- very-easy/check-if-an-array-includes-an-element/code.js
+++ very-easy/check-if-an-array-includes-an-element/code.js
@@ -1,5 +1,7 @@
 function check(arr, el) {
   // Your code here.
+  // `includes` metodidan foydalanib, element bor yoki yo'qligini tekshiramiz
+  return arr.includes(el);
 }
 
 module.exports = check;
