4e8a472d0348f3d393201d9cfad38cbe8c15580c
Lutfulla <155001736+LutfullaSamigullayev@users.noreply.github.com>
2025-06-12 08:30:21 +0500

    very-easy check-if-a-number-is-a-palindrome

--- very-easy/check-if-a-number-is-a-palindrome/code.js
+++ very-easy/check-if-a-number-is-a-palindrome/code.js
@@ -1,7 +1,7 @@
 function isPalindrome(n) {
   // Your code here.
    // Sonni stringga o‘girib, teskari versiyasi bilan solishtiramiz
-   return num.toString() === num.toString().split('').reverse().join('');
+   return n.toString() === n.toString().split('').reverse().join('');
 }
 
 module.exports = isPalindrome;
