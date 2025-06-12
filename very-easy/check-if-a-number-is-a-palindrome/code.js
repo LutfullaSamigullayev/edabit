e350328a5306fed84cde105cdc0868a5b1f905b2
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-12 08:22:31 +0500

    very-easy check-if-a-number-is-a-palindrome

--- very-easy/check-if-a-number-is-a-palindrome/code.js
+++ very-easy/check-if-a-number-is-a-palindrome/code.js
@@ -1,5 +1,7 @@
 function isPalindrome(n) {
   // Your code here.
+   // Sonni stringga o‘girib, teskari versiyasi bilan solishtiramiz
+   return num.toString() === num.toString().split('').reverse().join('');
 }
 
 module.exports = isPalindrome;
