365868116f8d39f6b5347508338d87ea15f3f28b
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-12 08:36:57 +0500

    very-easy check-if-a-string-contains-only-identical-characters

--- very-easy/check-if-a-string-contains-only-identical-characters/code.js
+++ very-easy/check-if-a-string-contains-only-identical-characters/code.js
@@ -1,5 +1,7 @@
 function isIdentical(s) {
   // Your code here.
+  // Har bir belgini birinchi belgi bilan solishtiramiz
+  return s.split('').every(char => char === s[0]);
 }
 
 module.exports = isIdentical;
