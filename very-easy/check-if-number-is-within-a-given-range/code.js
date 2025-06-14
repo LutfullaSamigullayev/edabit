2d935b6c5876245ff45e9b24fcf342cdfd9034de
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-14 09:13:26 +0500

    very-easy check-if-number-is-within-a-given-range

--- very-easy/check-if-number-is-within-a-given-range/code.js
+++ very-easy/check-if-number-is-within-a-given-range/code.js
@@ -1,5 +1,7 @@
 function isInRange(num, range) {
   // Your code here.
+  // Sonning range.min va range.max oralig‘ida ekanligini tekshiramiz
+  return num >= range.min && num <= range.max;
 }
 
 module.exports = isInRange;
