c371eab68499636ac20334f6fcc557fd4559c2ae
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-14 09:18:59 +0500

    very-easy check-if-string-ending-matches-second-string

--- very-easy/check-if-string-ending-matches-second-string/code.js
+++ very-easy/check-if-string-ending-matches-second-string/code.js
@@ -1,5 +1,7 @@
 function checkEnding(str1, str2) {
   // Your code here.
+  // str1 oxiri str2 bilan tugashini endsWith yordamida tekshiramiz
+  return str1.endsWith(str2);
 }
 
 module.exports = checkEnding;
