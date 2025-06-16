155fdd5c0e79dfc45740ded061eb924662b2118f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-16 19:16:48 +0500

    very-easy check-if-the-same-case

--- very-easy/check-if-the-same-case/code.js
+++ very-easy/check-if-the-same-case/code.js
@@ -1,5 +1,7 @@
 function sameCase(str) {
   // Your code here.
+  // Stringni to'liq kichik harfga va to'liq katta harfga o'tkazib taqqoslaymiz
+  return str === str.toLowerCase() || str === str.toUpperCase();
 }
 
 module.exports = sameCase;
