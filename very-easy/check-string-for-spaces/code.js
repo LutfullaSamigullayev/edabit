a4b8aac2c61e61d7714d6a76a3842d269069ab2d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-16 19:17:33 +0500

    very-easy check-string-for-spaces

--- very-easy/check-string-for-spaces/code.js
+++ very-easy/check-string-for-spaces/code.js
@@ -1,5 +1,7 @@
 function hasSpaces(str) {
   // Your code here.
+  // Stringni tekshiramiz, agar unda bo'sh joy (space) bo'lsa, true qaytaramiz
+  return str.indexOf(' ') !== -1;
 }
 
 module.exports = hasSpaces;
