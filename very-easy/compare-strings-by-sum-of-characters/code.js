014ca9013f5a3a2a2749b6a0d8885934b44cfea6
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-16 19:18:32 +0500

    very-easy compare-strings-by-sum-of-characters

--- very-easy/compare-strings-by-sum-of-characters/code.js
+++ very-easy/compare-strings-by-sum-of-characters/code.js
@@ -1,5 +1,8 @@
 function comp(str1, str2) {
   // Your code here.
+  // Har ikkala stringning uzunligini solishtiramiz
+  // Agar uzunliklar teng bo‘lsa, true qaytadi
+  return str1.length === str2.length;
 }
 
 module.exports = comp;
