69719be1dc577405b2e7460c4d02c8870b1365ec
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-17 09:49:08 +0500

    very-easy convert-number-to-corresponding-month-name

--- very-easy/convert-number-to-corresponding-month-name/code.js
+++ very-easy/convert-number-to-corresponding-month-name/code.js
@@ -1,5 +1,13 @@
 function month_name(num) {
   // Your code here.
+  // 12 ta oy nomini arrayda saqlaymiz
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  // Index 0 dan boshlanadi, shuning uchun 1 ni ayrib olamiz
+  return months[num - 1];
 }
 
 module.exports = month_name;
