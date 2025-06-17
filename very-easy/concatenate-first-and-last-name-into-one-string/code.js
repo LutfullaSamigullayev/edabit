34c073e92b52ada2e34a18cdc10f0fa21d466b5c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-17 08:19:32 +0500

    very-easy concatenate-first-and-last-name-into-one-string

--- very-easy/concatenate-first-and-last-name-into-one-string/code.js
+++ very-easy/concatenate-first-and-last-name-into-one-string/code.js
@@ -1,5 +1,7 @@
 function concatName(firstName, lastName) {
   // Your code here.
+  // Familiya va ismni vergul va bo'sh joy bilan birlashtirib qaytaramiz
+  return lastName + ", " + firstName;
 }
 
 module.exports = concatName;
