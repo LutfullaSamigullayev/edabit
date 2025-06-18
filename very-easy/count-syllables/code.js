a82e40384f972feeb4320e0b81f8ea6b3c85be2c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-18 12:46:21 +0500

    very-easy count-syllables

--- very-easy/count-syllables/code.js
+++ very-easy/count-syllables/code.js
@@ -1,5 +1,7 @@
 function numberSyllables(word) {
   // Your code here.
+  // "-" belgisi bo'yicha bo'lib, nechta qismga ajralganini sanaymiz
+  return word.split("-").length;
 }
 
 module.exports = numberSyllables;
