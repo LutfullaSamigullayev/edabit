1b247e87f0230a5ba4846141645d10536e0f47b8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-19 18:59:26 +0500

    very-easy difference-of-max-and-min-numbers-in-array

--- very-easy/difference-of-max-and-min-numbers-in-array/code.js
+++ very-easy/difference-of-max-and-min-numbers-in-array/code.js
@@ -1,5 +1,9 @@
 function differenceMaxMin(arr) {
   // Your code here.
+  // Math.max ga arr ni yoyib uning uchidan kattasini olamiz
+  // va Math.mim ga ham arr ni yoyib uning uchidan kichikini olamiz
+  // va ular o'rtasidagi farqni return qilamiz
+  return Math.max(...arr) - Math.min(...arr)
 }
 
 module.exports = differenceMaxMin;
