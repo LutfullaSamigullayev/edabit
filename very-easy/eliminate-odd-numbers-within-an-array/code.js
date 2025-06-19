d7af866d4ef6c858f9cf296c8bb88210e912608d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-19 19:11:59 +0500

    very-easy eliminate-odd-numbers-within-an-array

--- very-easy/eliminate-odd-numbers-within-an-array/code.js
+++ very-easy/eliminate-odd-numbers-within-an-array/code.js
@@ -1,5 +1,8 @@
 function noOdds(arr) {
   // Your code here.
+  // arr ni filter qilib har bir raqamni 2 bo'lganda qoldiq 0 bo'lsa
+  // demak u juft raqam, filter faqat shularni yangi arrayga o'tkazadi
+  return arr.filter(num => num % 2 == 0)
 }
 
 module.exports = noOdds;
