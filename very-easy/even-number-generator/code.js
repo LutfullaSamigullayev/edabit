8f09abdecc3c29ad3b692f88dec6b1b5cb147a55
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-19 19:27:16 +0500

    very-easy even-number-generator

--- very-easy/even-number-generator/code.js
+++ very-easy/even-number-generator/code.js
@@ -1,5 +1,16 @@
 function findEvenNums(num) {
   // Your code here.
+  let evens = []
+  // bu yerga juft sonlarni push qilib boramiz
+
+  // for ni 2 dan sanab boshlaymiz va uni evens ga push qilib boramiz
+  // va siklni 2 ga oshirib boramiz shunda faqat juft sonlar bo'ladi
+  for(let i = 2; i <= num; i += 2) {
+    evens.push(i)
+  }
+
+  // array ni return qilamiz
+  return evens
 }
 
 module.exports = findEvenNums;
