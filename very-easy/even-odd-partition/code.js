2451b72f829cdeaf99208be151bf623e0c5dee8a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-22 21:15:55 +0500

    very-easy even-odd-partition

--- very-easy/even-odd-partition/code.js
+++ very-easy/even-odd-partition/code.js
@@ -1,5 +1,18 @@
 function evenOddPartition(arr) {
   // Your code here.
+
+  const evens = [] // bu yerga juftlarni saqlab boramiz
+  const odds = [] // bu yerga toqlarni saqlab boramiz
+
+  arr.forEach(num => { // arr ni forEach qilib har bir raqamni tekshiramiz
+    if(num % 2 === 0) { // agar raqamni 2 ga qoldiqli bo'lganda 0 ga teng bo'lsa 
+      evens.push(num) // uni juftlarga massiviga push qilamiz
+    } else { // aks holda toqlar massiviga push qilamiz
+      odds.push(num)
+    }
+  })
+
+  return [evens, odds] // ikkala massivni bir qilib yuboramiz
 }
 
 module.exports = evenOddPartition;
