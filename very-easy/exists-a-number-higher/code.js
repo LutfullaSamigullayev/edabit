07a21304ba3e48e887aa2fb38d1cd4dae17b065e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-23 15:04:00 +0500

    very-easy exists-a-number-higher

--- very-easy/exists-a-number-higher/code.js
+++ very-easy/exists-a-number-higher/code.js
@@ -1,5 +1,8 @@
 function existsHigher(arr, n) {
   // Your code here.
+  // Arraydagi har bir elementni tekshiramiz:
+  // Agar hech bo‘lmaganda bittasi n dan katta yoki teng bo‘lsa => true
+  return arr.some((num) => num >= n);
 }
 
 module.exports = existsHigher;
