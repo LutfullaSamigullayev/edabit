9e7e52923ca6be526c288365f72b347c2050af7c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-23 15:13:07 +0500

    very-easy filter-by-digit-length

--- very-easy/filter-by-digit-length/code.js
+++ very-easy/filter-by-digit-length/code.js
@@ -1,5 +1,7 @@
 function filterDigitLength(arr, num) {
   // Your code here.
+  // Har bir sonni stringga aylantirib, uning uzunligini tekshiramiz
+  return arr.filter(n => n.toString().length === num);
 }
 
 module.exports = filterDigitLength;
