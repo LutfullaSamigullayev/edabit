b516937061a38dfd2df8d14c359aa12496c83674
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-24 13:26:04 +0500

    very-easy find-the-smallest-number-in-an-array

--- very-easy/find-the-smallest-number-in-an-array/code.js
+++ very-easy/find-the-smallest-number-in-an-array/code.js
@@ -1,5 +1,8 @@
 function findSmallestNum(arr) {
   // Your code here.
+  // Math.min ga arr ni ... orqali yoyib beramiz
+  // shularni ichidan eng kichikini return qilamiz
+  return Math.min(...arr)
 }
 
 module.exports = findSmallestNum;
