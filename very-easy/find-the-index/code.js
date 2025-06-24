f144cbd7c2c8844f41d2156cbfe306d0d5a5872d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-24 13:13:43 +0500

    very-easy find-the-index

--- very-easy/find-the-index/code.js
+++ very-easy/find-the-index/code.js
@@ -1,5 +1,7 @@
 function find_index(arr, str) {
   // Your code here.
+  // Arrayning indexOf metodi orqali berilgan satrning indexini olamiz
+  return arr.indexOf(str) // va uning index raqamini return qilamiz
 }
 
 module.exports = find_index;
