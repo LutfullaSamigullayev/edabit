0f972ce92d351a0b3671d8db0630cfb5c97ec6d5
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-24 13:22:53 +0500

    very-easy find-the-largest-number-in-an-array

--- very-easy/find-the-largest-number-in-an-array/code.js
+++ very-easy/find-the-largest-number-in-an-array/code.js
@@ -1,5 +1,8 @@
 function findLargestNum(arr) {
   // Your code here.
+  // Math.max ga arr ni ... orqali yoyib beramiz
+  // shularni ichidan eng kattasini return qilamiz
+  return Math.max(...arr)
 }
 
 module.exports = findLargestNum;
