0cfcabd3b245b46e0a47004c39deb567e050b269
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-28 10:00:29 +0500

    very-easy flip-the-boolean

--- very-easy/flip-the-boolean/code.js
+++ very-easy/flip-the-boolean/code.js
@@ -1,5 +1,11 @@
 function reverse(bool) {
   // Your code here.
+  // Agar qiymat boolean bo‘lsa, teskari qiymatini qaytaramiz
+  if (typeof bool === "boolean") {
+    return !bool;
+  }
+  // Aks holda, xatolik matnini qaytaramiz
+  return "boolean expected";
 }
 
 module.exports = reverse;
