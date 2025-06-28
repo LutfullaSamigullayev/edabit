11ed40dd6081211ca4af53aa35489c518f92ef65
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-28 13:26:26 +0500

    very-easy generate-a-countdown-of-numbers-in-an-array

--- very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
+++ very-easy/generate-a-countdown-of-numbers-in-an-array/code.js
@@ -1,5 +1,14 @@
 function countdown(start) {
   // Your code here.
+  // Natija uchun bo'sh array yaratamiz
+   const result = [];
+
+   // num dan 0 gacha kamayib boramiz
+   for (let i = start; i >= 0; i--) {
+     result.push(i); // Har bir sonni arrayga qo‘shamiz
+   }
+ 
+   return result;
 }
 
 module.exports = countdown;
