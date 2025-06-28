9141cabe08d626d41d0abde88ad838cd007c838d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-06-28 09:49:36 +0500

    very-easy fix-the-error-vowel-edition

--- very-easy/fix-the-error-vowel-edition/code.js
+++ very-easy/fix-the-error-vowel-edition/code.js
@@ -1,5 +1,7 @@
 function removeVowels(str) {
   // Your code here.
+  // Barcha unli harflarni global tarzda almashtiramiz
+  return str.replace(/[aeiou]/g, "");
 }
 
 module.exports = removeVowels;
