d8b75eaa2f3cbe9326846b0cff7ce82b3e751f2c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-01 11:12:31 +0500

    very-easy hamming-distance

--- very-easy/hamming-distance/code.js
+++ very-easy/hamming-distance/code.js
@@ -1,5 +1,14 @@
 function hammingDistance(str1, str2) {
   // Your code here.
+  let count = 0 // farqlarni hisoblab borish uchun o'zgaruvchi ochamiz
+  // keyin for orqali str1 va str2 larning har birini indexi bo'yicha
+  // solishtirib chiqamiz
+  for(let i = 0; i < str1.length; i++) {
+    if(str1[i] !== str2[i]) { // agar ular bir biridan farq qilsa
+      count++ // countni 1 ga oshiramiz
+    }
+  }
+  return count // count ni return qilamiz
 }
 
 module.exports = hammingDistance;
