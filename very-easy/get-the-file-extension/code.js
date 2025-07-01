884e60d37e5f3a598c1d33d0a091d0abe31281ee
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-01 11:11:57 +0500

    very-easy get-the-file-extension

--- very-easy/get-the-file-extension/code.js
+++ very-easy/get-the-file-extension/code.js
@@ -1,5 +1,9 @@
 function getExtension(arr) {
   // Your code here.
+  return arr.map(word => word.split('.')[1])
+  // arr ni map qilib har bir kelgan word ni split metodi bilan
+  // '.'-nuqta bo'yicha bo'lakga ajaratamiz
+  // va 1-indexdagi matn ni return qilamiz
 }
 
 module.exports = getExtension;
