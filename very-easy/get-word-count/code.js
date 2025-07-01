8c5d8a4f71893e301325c4ef60f5b087856ece28
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-01 11:12:16 +0500

    very-easy get-word-count

--- very-easy/get-word-count/code.js
+++ very-easy/get-word-count/code.js
@@ -1,5 +1,10 @@
 function countWords(str) {
   // Your code here.
+  // split metodi bilan ' '-bo'sh jaylar orqali bo'laklarga bo'lamiz
+  // va hosil bo'lgan arrayning length orqali 
+  // ichida qancha so'z bo'rligini return qilamiz
+  
+  return str.split(' ').length
 }
 
 module.exports = countWords;
