226adf47d77fb8df013332c92d9e8e93f45649bf
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-02 23:11:12 +0500

    very-easy is-the-number-even-or-odd

--- very-easy/is-the-number-even-or-odd/code.js
+++ very-easy/is-the-number-even-or-odd/code.js
@@ -1,5 +1,8 @@
 function isEvenOrOdd(num) {
   // Your code here.
+  if(num % 2 === 0) { // agar num ni 2 ga qoldiqli bo'lganda
+    return "even" // qoldiq 0 ga teng bo'lsa demak u juft
+  } else return "odd" // aks holda u toq 
 }
 
 module.exports = isEvenOrOdd;
