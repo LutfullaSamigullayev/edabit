afd1eb7e9949b38cd6d479c30f9570eeaecfeee4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-02 23:08:31 +0500

    very-easy hurdle-jump

--- very-easy/hurdle-jump/code.js
+++ very-easy/hurdle-jump/code.js
@@ -1,5 +1,10 @@
 function hurdleJump(hurdles, jumpHeight) {
   // Your code here.
+  // hurdles ni every metodi orqali har birini 
+  // jumpHeight bilan solishtirib chiqamiz
+  // agar barchasi jumpHeight dan kichik bo'lsa true 
+  // aks holda 1tasi katta bo'lsa ham false ni return qiladi
+  return hurdles.every(num => jumpHeight >= num)
 }
 
 module.exports = hurdleJump;
