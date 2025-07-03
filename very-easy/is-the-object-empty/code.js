ce990f6b263ab3cc39807ed8fc18498d7681edb7
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-03 16:34:38 +0500

    very-easy is-the-object-empty

--- very-easy/is-the-object-empty/code.js
+++ very-easy/is-the-object-empty/code.js
@@ -1,5 +1,10 @@
 function isEmpty(obj) {
   // Your code here.
+  // obj ichidagi elementlarni array ko'rinishiga keltiramiz
+  // keyin uning uzunligini tekshiramiz agar ichi bo'sh bo'lsa true
+  // aks holda false qaytaramiz
+  return Object.keys(obj).length === 0
 }
 
+console.log(isEmpty({ a: 1 }))
 module.exports = isEmpty;
