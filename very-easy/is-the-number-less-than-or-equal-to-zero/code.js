4c9a166b8705e9b9a772162d8d8f2ef4e7cb7288
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-03 16:29:47 +0500

    very-easy is-the-number-less-than-or-equal-to-zero

--- very-easy/is-the-number-less-than-or-equal-to-zero/code.js
+++ very-easy/is-the-number-less-than-or-equal-to-zero/code.js
@@ -1,5 +1,7 @@
 function lessThanOrEqualToZero(num) {
   // Your code here.
+  // agar num noldan kichik yoki teng bo'lsa true ni return qiladi
+  return num <= 0
 }
 
 module.exports = lessThanOrEqualToZero;
