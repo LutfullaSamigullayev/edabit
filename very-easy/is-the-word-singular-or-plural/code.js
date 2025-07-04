3e5f5c0c2aae6a2668b114c6c7f2eb6fc927ef55
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-04 18:47:50 +0500

    very-easy is-the-word-singular-or-plural

--- very-easy/is-the-word-singular-or-plural/code.js
+++ very-easy/is-the-word-singular-or-plural/code.js
@@ -1,5 +1,7 @@
 function isPlural(word) {
   // Your code here.
+  // word ning oxirgi harfini olib uni s bilan solishtiramiz
+  return word[word.length - 1] === 's'
 }
 
 module.exports = isPlural;
