9789e4f52310608f3ad6a87c19d37637052a3831
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-05 09:55:33 +0500

    very-easy lexicographically-first-and-last

--- very-easy/lexicographically-first-and-last/code.js
+++ very-easy/lexicographically-first-and-last/code.js
@@ -1,5 +1,16 @@
 function firstAndLast(s) {
   // Your code here.
+  // Oldin s ni bo'laklarga bo'lib sortlab olamiz
+  // va uni first ga saqlaymiz
+  const first = s.split("").sort()
+  
+  // keyin hosil bo'lgan tartiblangan so'zni 
+  // slice orqali nusxalab olamiz first o'zgarib ketmasligi uchun
+  // uni reverse teskarisiga aylantirib last ga saqlaymiz
+  const last = first.slice().reverse()
+  
+  // ularni join orqali birlashtirib string qilamiz
+  return [first.join(''), last.join('')];
 }
 
 module.exports = firstAndLast;
