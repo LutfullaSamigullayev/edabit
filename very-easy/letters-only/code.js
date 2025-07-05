58c20a4a7f44ceaa750e43b06375cefddf97549d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-05 09:34:27 +0500

    very-easy letters-only

--- very-easy/letters-only/code.js
+++ very-easy/letters-only/code.js
@@ -1,5 +1,11 @@
 function lettersOnly(str) {
   // Your code here.
+  // Faqat harflarni qoldirish uchun, harfdan boshqa barcha belgilarni olib tashlaymiz
+  return str.replace(/[^a-z]/gi, '')
+  // `/[^a-z]/gi` — harfdan tas hqari barcha belgilarni topadi (`^` - inkor),
+  // `g` — global qidiruv (hamma joydan),
+  // `i` — katta-kichik harfni farqlamaydi.
+  // "" — topilgan belgilarni bo‘sh joy bilan almashtiradi (ya'ni olib tashlaydi).
 }
 
 module.exports = lettersOnly;
