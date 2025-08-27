77e33e23f350328db37c39ce1450510087cc8d05
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-08-27 11:06:28 +0500

    very-easy little-dictionary

--- very-easy/little-dictionary/code.js
+++ very-easy/little-dictionary/code.js
@@ -1,5 +1,10 @@
 function dictionary(initial, words) {
   // Your code here.
+  // words ni filter qilib startsWith orqali 
+  // boshlang'ich qismi bilan filterlab olamiz
+  // filter o'zi yangi array qaytaradi 
+  // shuni return qilamiz
+  return words.filter(word => word.startsWith(initial))
 }
 
 module.exports = dictionary;
