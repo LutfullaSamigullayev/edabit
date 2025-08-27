b49f896b69c64a161d377061611097ea8449df21
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-08-27 11:56:32 +0500

    very-easy minimum-removals-to-make-sum-even

--- very-easy/minimum-removals-to-make-sum-even/code.js
+++ very-easy/minimum-removals-to-make-sum-even/code.js
@@ -1,5 +1,12 @@
 function minimumRemovals(arr) {
   // Your code here.
+  // oldim arr ichidagi elementlarni barchasini qo'shamiz
+  const summ = arr.reduce((a,b) => a+b)
+  // summ juft bo'lsa 0 ni return qilamiz
+  if(summ % 2 == 0) return 0
+  // aks holda 1 ni return qilamiz
+  // (yani 1 ta elementni olib tashlash kerak)
+  return 1
 }
 
 module.exports = minimumRemovals;
