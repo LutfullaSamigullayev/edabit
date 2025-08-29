1d2858ed611b3a8d8cd006f1c814fa0c31a03cc3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-08-29 17:55:40 +0500

    very-easy mirror-array

--- very-easy/mirror-array/code.js
+++ very-easy/mirror-array/code.js
@@ -1,5 +1,9 @@
 function mirror(arr) {
   // Your code here.
+  // arr oxirgi elementini qoldirib
+  // nusxalab uni teskari aylantirib 
+  // arr ga qo'shamiz va return qilamiz
+  return arr.concat(arr.slice(0, -1).reverse())
 }
 
 module.exports = mirror;
