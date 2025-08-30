4445417bd354e4915c0a7dfb113e92e27cfe5f4c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-08-30 10:34:58 +0500

    very-easy multiply-by-length

--- very-easy/multiply-by-length/code.js
+++ very-easy/multiply-by-length/code.js
@@ -1,5 +1,9 @@
 function MultiplyByLength(arr) {
   // Your code here.
+  // arr ni map orqali aylaning 
+  // har bir sonni uning uzunligiga ko'paytiramiz
+  // yangi arrayni return qilamiz
+  return arr.map(num => num * arr.length)
 }
 
 module.exports = MultiplyByLength;
