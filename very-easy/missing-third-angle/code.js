1e5d83c68f39e52c35868ea8cb492325a2c5bc77
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-08-30 10:21:31 +0500

    very-easy missing-third-angle

--- very-easy/missing-third-angle/code.js
+++ very-easy/missing-third-angle/code.js
@@ -1,5 +1,12 @@
 function missingAngle(angle1, angle2) {
   // Your code here.
+  // Uchinchi burchakni topamiz
+  const c = 180 - (angle1 + angle2);
+
+  // Burchakni tasniflaymiz
+  if (c < 90) return "acute";
+  if (c === 90) return "right";
+  return "obtuse";
 }
 
 module.exports = missingAngle;
