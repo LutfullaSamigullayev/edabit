8ef816cad78fee0f23a6c7314da6a367b30859e4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-04 15:12:16 +0500

    very-easy next-element-in-arithmetic-sequence

--- very-easy/next-element-in-arithmetic-sequence/code.js
+++ very-easy/next-element-in-arithmetic-sequence/code.js
@@ -1,5 +1,10 @@
 function nextElement(arr) {
   // Your code here.
+   // Birinchi ikki elementdan farqni topamiz
+   const diff = arr[1] - arr[0];
+
+   // Oxirgi elementga shu farqni qo‘shamiz
+   return arr[arr.length - 1] + diff;
 }
 
 module.exports = nextElement;
