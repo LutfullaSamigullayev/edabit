de9107949801597701e93519baabad3c736cd28a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-04 15:44:54 +0500

    very-easy nth-smallest-element

--- very-easy/nth-smallest-element/code.js
+++ very-easy/nth-smallest-element/code.js
@@ -1,5 +1,13 @@
 function nthSmallest(arr, n) {
   // Your code here.
+    // Massivni tartiblaymiz
+    const sorted = arr.sort((a, b) => a - b);
+
+    // Agar n massiv uzunligidan katta bo‘lsa, null qaytaramiz
+    if (n - 1 > sorted.length) return null;
+  
+    // n-chi eng kichik elementni qaytaramiz
+    return sorted[n - 1]
 }
 
 module.exports = nthSmallest;
