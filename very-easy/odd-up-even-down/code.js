453020d0590680bbc8afe394f8873780c902d157
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-07 15:40:26 +0500

    very-easy odd-up-even-down

--- very-easy/odd-up-even-down/code.js
+++ very-easy/odd-up-even-down/code.js
@@ -1,5 +1,8 @@
 function transform(arr) {
   // Your code here.
+  // arr ni map orqali aylanib juftlarni 1 ga kamaytirib 
+  // toqlarni 1 ga oshirib yangi arrayni return qilamiz
+  return arr.map(num => num % 2 === 0 ? num - 1 : num + 1);
 }
 
 module.exports = transform;
