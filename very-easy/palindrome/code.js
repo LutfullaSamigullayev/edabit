749a7a6b6863206df03a15aba1833d7e30b4e1d0
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-10 16:28:19 +0500

    very-easy palindrome

--- very-easy/palindrome/code.js
+++ very-easy/palindrome/code.js
@@ -1,5 +1,9 @@
 function checkPalindrome(str) {
   // Your code here.
+  // str ni bo'laklarga ajratamiz,
+  // keyin uni teskari tartibda qo'shamiz
+  // va asl str bilan solishtiramiz
+  return str.split('').reverse().join('') === str;
 }
 
 module.exports = checkPalindrome;
