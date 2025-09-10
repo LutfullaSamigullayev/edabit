ec0e5824b286dcac753528015226a403dad0ceee
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-10 16:39:09 +0500

    very-easy product-of-all-odd-integers

--- very-easy/product-of-all-odd-integers/code.js
+++ very-easy/product-of-all-odd-integers/code.js
@@ -1,5 +1,8 @@
 function oddProduct(arr) {
   // Your code here.
+  // oldin arr ni filter qilib faqat toq sonlarni olamiz
+  // keyin reduce qilib ularni ko'paytiramiz
+  return arr.filter(num => num % 2 !== 0).reduce((acc, num) => acc * num, 1);
 }
 
 module.exports = oddProduct;
