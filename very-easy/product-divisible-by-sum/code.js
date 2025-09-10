cf29d67e8baee0dc5dff678f8e729547cefd1da2
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-09-10 16:33:55 +0500

    very-easy product-divisible-by-sum

--- very-easy/product-divisible-by-sum/code.js
+++ very-easy/product-divisible-by-sum/code.js
@@ -1,5 +1,16 @@
 function divisible(arr) {
   // Your code here.
+  // arr ni yig'indisini hisoblaymiz
+  const sum = arr.reduce((a, b) => a + b, 0);
+  
+  // arr ni ko'paytmasini hisoblaymiz
+  const product = arr.reduce((a, b) => a * b, 1);
+
+  // oldin sumni 0 ga teng emasligini tekshiramiz
+  if (sum === 0) return false;
+
+  // ko'paytmani yig'indiga bo'lamiz va butun sonligini tekshiramiz
+  return product % sum === 0;
 }
 
 module.exports = divisible;
