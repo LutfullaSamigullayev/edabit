a79877a7068f915ac4244f027c5bd72e111fd111
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-01 20:49:36 +0500

    very-easy repeat-the-same-item-multiple-times

--- very-easy/repeat-the-same-item-multiple-times/code.js
+++ very-easy/repeat-the-same-item-multiple-times/code.js
@@ -1,5 +1,7 @@
 function repeat(item, times) {
   // Your code here.
+  // Array() va fill() metodidan foydalanib massiv hosil qilamiz
+  return Array(times).fill(item);
 }
 
 module.exports = repeat;
