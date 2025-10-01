f71d254a682323f0a844f0579b35f243bab5db62
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-01 20:35:57 +0500

    very-easy remove-null-from-an-array

--- very-easy/remove-null-from-an-array/code.js
+++ very-easy/remove-null-from-an-array/code.js
@@ -1,5 +1,7 @@
 function removeNull(arr) {
   // Your code here.
+  // filter() orqali null qiymatlarni chiqarib tashlaymiz
+  return arr.filter(item => item != null);
 }
 
 module.exports = removeNull;
