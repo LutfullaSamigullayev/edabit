ce6c8723030641af346b112849f87a1f12475ce3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-01 20:43:14 +0500

    very-easy remove-the-first-and-last-characters

--- very-easy/remove-the-first-and-last-characters/code.js
+++ very-easy/remove-the-first-and-last-characters/code.js
@@ -1,5 +1,10 @@
 function removeFirstLast(str) {
   // Your code here.
+   // Agar uzunlik <= 2 bo‘lsa, o‘zini qaytaramiz
+   if (str.length <= 2) return str;
+
+   // Aks holda birinchi va oxirgi belgini olib tashlaymiz
+   return str.slice(1, -1);
 }
 
 module.exports = removeFirstLast;
