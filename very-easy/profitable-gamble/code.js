259e78050395efa2e13ac0d2e6a4e551802e009f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-01 20:19:20 +0500

    very-easy profitable-gamble

--- very-easy/profitable-gamble/code.js
+++ very-easy/profitable-gamble/code.js
@@ -1,5 +1,7 @@
 function profitableGamble(prob, prize, pay) {
   // Your code here.
+  // Foyda sharti: agar ehtimol * mukofot > xarajat bo‘lsa
+  return prob * prize > pay;
 }
 
 module.exports = profitableGamble;
