006772dd251980b7d1b286e899c9ab7c4cc69875
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-04 08:50:11 +0500

    very-easy retrieve-the-subreddit

--- very-easy/retrieve-the-subreddit/code.js
+++ very-easy/retrieve-the-subreddit/code.js
@@ -1,5 +1,8 @@
 function subReddit(link) {
   // Your code here.
+  // link ni /r/ bo'yicha 2 ga ajratib olamiz 
+	// va 1-indexda turganni / belgisini olib tashlab return qilamiz
+  return link.split("/r/")[1].replace("/", "");
 }
 
 module.exports = subReddit;
