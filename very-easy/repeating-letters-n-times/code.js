34c736801caed83d962a63205757de2bd3527885
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-04 08:37:08 +0500

    very-easy repeating-letters-n-times

--- very-easy/repeating-letters-n-times/code.js
+++ very-easy/repeating-letters-n-times/code.js
@@ -1,5 +1,9 @@
 function repeat(str, n) {
   // Your code here.
+  // split → harflarga ajratamiz
+  // map → har birini n marta takrorlaymiz
+  // join → qayta stringga yig‘amiz
+  return str.split("").map(ch => ch.repeat(n)).join("");
 }
 
 module.exports = repeat;
