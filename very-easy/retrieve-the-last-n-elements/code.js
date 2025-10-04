1792058586f2598b305131e55f13215a0fac63c1
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-04 08:45:27 +0500

    very-easy retrieve-the-last-n-elements

--- very-easy/retrieve-the-last-n-elements/code.js
+++ very-easy/retrieve-the-last-n-elements/code.js
@@ -1,5 +1,8 @@
 function last(a, n) {
   // Your code here.
+  if (n > a.length) return "invalid";  // uzunlikdan katta bo'lsa
+  if (n === 0) return [];                // agar n = 0 bo'lsa
+  return a.slice(-n);                  // oxirgi n element
 }
 
 module.exports = last;
