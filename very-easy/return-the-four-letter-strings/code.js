3fb76d713b7642e866442e6975617fb038f370ad
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-20 15:01:51 +0500

    very-easy return-the-four-letter-strings

--- very-easy/return-the-four-letter-strings/code.js
+++ very-easy/return-the-four-letter-strings/code.js
@@ -1,5 +1,8 @@
 function isFourLetters(arr) {
   // Your code here.
+  // filter orqali har bir so'zning uzunligini tekshirib
+  // 4 ga teng bo'lganlarni olamiz
+  return arr.filter(word => word.length === 4); 
 }
 
 module.exports = isFourLetters;
