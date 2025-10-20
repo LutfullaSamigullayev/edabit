f91a3bf51459314c1b3516d40efb92f5c100a373
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-20 15:59:03 +0500

    very-easy return-the-total-number-of-parameters

--- very-easy/return-the-total-number-of-parameters/code.js
+++ very-easy/return-the-total-number-of-parameters/code.js
@@ -1,5 +1,7 @@
 function numberArgs() {
   // Your code here.
+  // ...args — bu barcha argumentlarni massiv sifatida qabul qiladi
+  return args.length; // massiv uzunligini qaytaradi, ya’ni nechta argument berilganini
 }
 
 module.exports = numberArgs;
