60ae7be31e829d658fae76fba3f692b3eca5cb1d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-20 15:08:20 +0500

    very-easy return-the-last-item-in-an-array

--- very-easy/return-the-last-item-in-an-array/code.js
+++ very-easy/return-the-last-item-in-an-array/code.js
@@ -1,5 +1,7 @@
 function getLastItem(arr) {
   // Your code here.
+  // arrayning uzunligidan 1 ni ayirsak uning oxirgi indexi bo'ladi
+  return arr[arr.length - 1];
 }
 
 module.exports = getLastItem;
