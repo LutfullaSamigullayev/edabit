7d95c65c15e228d6fdec792ad57129928a6c87c3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-20 14:58:08 +0500

    very-easy return-the-first-and-last-elements-in-an-array

--- very-easy/return-the-first-and-last-elements-in-an-array/code.js
+++ very-easy/return-the-first-and-last-elements-in-an-array/code.js
@@ -1,5 +1,8 @@
 function firstLast(arr) {
   // Your code here.
+  // at metodi bilan berilgan indexni olamiz 
+  // unga manfiy index berilsa oxirdan boshlab hisoblaydi
+  return [arr.at(0), arr.at(-1)];
 }
 
 module.exports = firstLast;
