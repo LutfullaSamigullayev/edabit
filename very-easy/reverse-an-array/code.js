c24b8667c9a1deb3195252c6e408699dc514522f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-24 18:56:58 +0500

    very-easy reverse-an-array

--- very-easy/reverse-an-array/code.js
+++ very-easy/reverse-an-array/code.js
@@ -1,5 +1,14 @@
 function reverse(arr) {
   // Your code here.
+  let res = [];
+  // natijlarni saqlash uchun bo'sh array yaratamiz
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // arrayni teskari tartibda aylanamiz
+    res.push(arr[i]);
+    // har bir elementni yangi arrayga qo'shamiz
+  }
+  // res arrayni return qilamiz
+  return res;
 }
 
 module.exports = reverse;
