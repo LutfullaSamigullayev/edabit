db24357ba8dca07b422cf2e7ef406f74cc6d61a8
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-29 10:36:43 +0500

    very-easy reverse-the-order-of-a-string

--- very-easy/reverse-the-order-of-a-string/code.js
+++ very-easy/reverse-the-order-of-a-string/code.js
@@ -1,5 +1,10 @@
 function reverse(str) {
   // Your code here.
+  // str ni split('') bilan bo'laklarga bo'lib array qilamiz
+  // hosil bo'lgan arrayni reverse() metodi orqali teskari qilamiz
+  // hosil bo'lgan teskari arrayni join('') orqalli birlashtiramiz
+  // hosil bo'lgan stringni return qilamiz
+  return str.split('').reverse().join('')
 }
 
 module.exports = reverse;
