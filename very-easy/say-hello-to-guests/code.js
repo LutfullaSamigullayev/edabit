41469fe4e2e1fc5cf476469d29b4b57049ed3059
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-29 10:54:06 +0500

    very-easy say-hello-to-guests

--- very-easy/say-hello-to-guests/code.js
+++ very-easy/say-hello-to-guests/code.js
@@ -1,5 +1,9 @@
 function greetPeople(names) {
   // Your code here.
+  // names array ni map orqali har bir ism oldiga hello qo'shib
+  // hosil bo'lgan yangi array ni join orqali orasiga vargul va bosh joy qo'yib
+  // string qilib return qilamiz
+  return names.map(name => "Hello " + name).join(", ")
 }
 
 module.exports = greetPeople;
