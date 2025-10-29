14bab9a05bcf253e70a6f5ba8d5ffc8e2539496f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-29 10:49:04 +0500

    very-easy same-number-of-unique-elements

--- very-easy/same-number-of-unique-elements/code.js
+++ very-easy/same-number-of-unique-elements/code.js
@@ -1,5 +1,11 @@
 function same(a1, a2) {
   // Your code here.
+  // Har bir massivdagi unikal elementlarni Set yordamida topamiz
+  const unique1 = new Set(a1);
+  const unique2 = new Set(a2);
+
+  // Set obyektining size xossasi unikal elementlar sonini bildiradi
+  return unique1.size === unique2.size;
 }
 
 module.exports = same;
