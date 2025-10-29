e3b915193f866fb76fc5ec8ce78c61c73e3b8e10
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-29 10:31:41 +0500

    very-easy reverse-coding-challenge-3

--- very-easy/reverse-coding-challenge-3/code.js
+++ very-easy/reverse-coding-challenge-3/code.js
@@ -1,5 +1,8 @@
 function mysteryFunc(arr, num) {
   // Your code here.
+  // arr ni map qilib har bir sonni num ga qoldiqli bo'lib
+  // uning qoldiqlarini array qilib return qilamiz
+  return arr.map(e => e % num)
 }
 
 module.exports = mysteryFunc;
