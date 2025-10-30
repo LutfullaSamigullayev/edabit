974e5b8d92dc71a61e7c300e8c12a6ba2c8f1f24
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-30 23:27:44 +0500

    very-easy smaller-string-number

--- very-easy/smaller-string-number/code.js
+++ very-easy/smaller-string-number/code.js
@@ -1,5 +1,11 @@
 function smallerNum(n1, n2) {
   // Your code here.
+  // Avvalo, uzunlikni solishtiramiz — uzun son kattaroq bo'ladi
+  if (n1.length < n2.length) return n1;
+  if (n1.length > n2.length) return n2;
+
+  // Agar uzunliklar teng bo'lsa, leksik tartibda solishtiramiz
+  return n1 < n2 ? n1 : n2;
 }
 
 module.exports = smallerNum;
