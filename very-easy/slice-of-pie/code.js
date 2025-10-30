e40042a65b9b115f7555a099f261c9fa07704adc
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-30 23:24:42 +0500

    very-easy slice-of-pie

--- very-easy/slice-of-pie/code.js
+++ very-easy/slice-of-pie/code.js
@@ -1,5 +1,13 @@
 function equalSlices(total, people, each) {
   // Your code here.
+  // Agar odam bo'lmasa, pirogni bo'lish shart emas
+  if (people === 0) return true;
+
+  // Har bir odamga beriladigan jami bo'laklar sonini hisoblaymiz
+  const needed = people * each;
+
+  // Agar jami bo'laklar yetarli bo'lsa, true qaytaramiz
+  return needed <= total;
 }
 
 module.exports = equalSlices;
