61ebf681db568ce5e218729f412c6f32ad32091a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-10-30 23:21:38 +0500

    very-easy shuffle-the-name

--- very-easy/shuffle-the-name/code.js
+++ very-easy/shuffle-the-name/code.js
@@ -1,5 +1,10 @@
 function nameShuffle(str) {
   // Your code here.
+  // ism va familiyani bo'sh joydan ajratamiz
+  const parts = str.split(' ');
+
+  // joylarini almashtirib, qayta birlashtiramiz
+  return `${parts[1]} ${parts[0]}`;
 }
 
 module.exports = nameShuffle;
