8b26c08e9159f01aa97f8322b7061ff6eb743430
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 16:25:54 +0500

    very-easy volume-of-a-box

--- very-easy/volume-of-a-box/code.js
+++ very-easy/volume-of-a-box/code.js
@@ -1,5 +1,10 @@
 function volumeOfBox(sizes) {
   // Your code here.
+    // obyekt ichidan height, width va length qiymatlarini olamiz
+    const { width, length, height } = sizes;
+
+    // uchala o'lchamni ko'paytirib hajmni hisoblaymiz
+    return width * length * height;
 }
 
 module.exports = volumeOfBox;
