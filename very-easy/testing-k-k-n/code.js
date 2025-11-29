a09a25857e903df2b8255deb95d0d8709b5288db
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 16:01:10 +0500

    very-easy testing-k-k-n

--- very-easy/testing-k-k-n/code.js
+++ very-easy/testing-k-k-n/code.js
@@ -1,5 +1,8 @@
 function kToK(n, k) {
   // Your code here.
+  // agar k ning k ga darajasi n ga teng bo'lsa 
+  // true ni return qilamiz aks holda false
+  return n === k**k
 }
 
 module.exports = kToK;
