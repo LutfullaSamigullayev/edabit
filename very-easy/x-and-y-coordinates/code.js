e8998e42c6db12c52e447c8fe28dfbf7788f4f00
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 17:50:00 +0500

    very-easy x-and-y-coordinates

--- very-easy/x-and-y-coordinates/code.js
+++ very-easy/x-and-y-coordinates/code.js
@@ -1,5 +1,15 @@
 function convertCartesian(x, y) {
   // Your code here.
+  let result = [];
+  // Natijaviy koordinatalarni saqlash uchun bo'sh massiv
+
+  for (let i = 0; i < x.length; i++) {
+    // Har bir indeks bo'yicha x va y ni juftlaymiz
+    result.push([x[i], y[i]]);
+  }
+
+  // Hosil bo'lgan koordinatalar massivini qaytaramiz
+  return result;
 }
 
 module.exports = convertCartesian;
