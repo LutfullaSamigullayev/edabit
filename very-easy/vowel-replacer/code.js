5fcae9f064f0fe194aad3a0a09b12dc3591106ad
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 17:30:00 +0500

    very-easy vowel-replacer

--- very-easy/vowel-replacer/code.js
+++ very-easy/vowel-replacer/code.js
@@ -1,5 +1,9 @@
 function replaceVowels(str, ch) {
   // Your code here.
+  // Unli harflarni aniqlash uchun regex ishlatamiz
+  // a, e, i, o, u harflarini qidiramiz
+  return str.replace(/[aeiou]/g, ch);
+  // Topilgan barcha unli harflarni berilgan belgi bilan almashtiramiz
 }
 
 module.exports = replaceVowels;
