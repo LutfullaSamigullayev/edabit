41c998091b238cb900a519586939d2b0a2996489
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 17:40:00 +0500

    very-easy word-endings

--- very-easy/word-endings/code.js
+++ very-easy/word-endings/code.js
@@ -1,5 +1,16 @@
 function addEnding(arr, ending) {
   // Your code here.
+  let result = [];
+  // Natijalarni saqlash uchun bo'sh massiv yaratamiz
+
+  for (let i = 0; i < arr.length; i++) {
+    // Massivni boshidan oxirigacha aylanamiz
+    result.push(arr[i] + ending);
+    // Har bir so'z oxiriga qo'shimchani qo'shamiz
+  }
+
+  // Yangi hosil bo'lgan massivni return qilamiz
+  return result;
 }
 
 module.exports = addEnding;
