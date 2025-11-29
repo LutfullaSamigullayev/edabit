3cda62d475371495dadd8a2a9761671e0829fc5e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 16:01:36 +0500

    very-easy the-farm-problem

--- very-easy/the-farm-problem/code.js
+++ very-easy/the-farm-problem/code.js
@@ -1,5 +1,10 @@
 function animals(chickens, cows, pigs) {
   // Your code here.
+  // tovuqlarning sonini 2 ta oyoq ga ko'paytiramiz
+  // mollarning sonini 4 ta oyoq ga ko'paytiramiz
+  // chochqalarning sonini 4 ta oyoq ga ko'paytiramiz
+  // va barcha oyoqlarni sonini hisoblab chiqaramiz
+  return ((chickens * 2) + (cows * 4) + (pigs * 4))
 }
 
 module.exports = animals;
