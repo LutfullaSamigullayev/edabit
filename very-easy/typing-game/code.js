80e767c6ba6b1481577052529a06279b2f1cab69
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 16:22:49 +0500

    very-easy typing-game

--- very-easy/typing-game/code.js
+++ very-easy/typing-game/code.js
@@ -1,5 +1,17 @@
 function correctStream(user, correct) {
   // Your code here.
+  let result = [];
+
+  // Har bir indeks bo'yicha taqqoslaymiz
+  for (let i = 0; i < user.length; i++) {
+    if (user[i] === correct[i]) {
+      result.push(1);   // to'g'ri yozilgan
+    } else {
+      result.push(-1);  // noto'g'ri yozilgan
+    }
+  }
+
+  return result;
 }
 
 module.exports = correctStream;
