ed29e880aa6d0cccc2cf73c02693989fde4c5e53
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-11-29 16:19:20 +0500

    very-easy total-number-of-unique-characters

--- very-easy/total-number-of-unique-characters/code.js
+++ very-easy/total-number-of-unique-characters/code.js
@@ -1,5 +1,13 @@
 function countUnique(s1, s2) {
   // Your code here.
+  // Ikki stringni birlashtiramiz
+  const combined = s1 + s2;
+
+  // Set yordamida faqat noyob belgilarni olamiz
+  const uniqueChars = new Set(combined);
+
+  // Set uzunligi noyob belgilar soni bo'ladi
+  return uniqueChars.size;
 }
 
 module.exports = countUnique;
