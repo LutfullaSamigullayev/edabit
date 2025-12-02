dfda9f692d70d9447de951e92035b5dbcefdd7c0
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:47:00 +0500

    easy compare-by-ascii-codes

--- easy/compare-by-ascii-codes/code.js
+++ easy/compare-by-ascii-codes/code.js
@@ -1,5 +1,18 @@
 function asciiSort(arr) {
   // Your code here.
+  // So‘zning ASCII yig‘indisini hisoblaydigan yordamchi funksiya
+  function asciiSum(word) {
+    let sum = 0;
+
+    for (let char of word) {
+      sum += char.charCodeAt(0); // har bir harfning ASCII kodi
+    }
+
+    return sum;
+  }
+
+  // Ikki so‘zning ASCII yig‘indilarini solishtiramiz
+  return asciiSum(arr[0]) < asciiSum(arr[1]) ? arr[0] : arr[1];
 }
 
 module.exports = asciiSort;
