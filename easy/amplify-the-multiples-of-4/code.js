4f4e167a085ae7a75ec993a5f856b1fecac599af
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:42:00 +0500

    easy amplify-the-multiples-of-4

--- easy/amplify-the-multiples-of-4/code.js
+++ easy/amplify-the-multiples-of-4/code.js
@@ -1,5 +1,23 @@
 function amplify(num) {
   // Your code here.
+  let result = [];
+  // Natijalarni saqlash uchun bo'sh massiv yaratamiz
+
+  for (let i = 1; i <= num; i++) {
+    // 1 dan berilgan songacha aylanamiz
+    if (i % 4 === 0) {
+      // Agar son 4 ga qoldiqsiz bo'linsa
+      result.push(i * 10);
+      // Sonni 10 ga ko'paytirib qo'shamiz
+    } else {
+      // Aks holda
+      result.push(i);
+      // Sonning o'zini qo'shamiz
+    }
+  }
+
+  // Yakuniy massivni return qilamiz
+  return result;
 }
 
 module.exports = amplify;
