d65e2fad44bedaf3615361d2bd7c3f60cca8b9df
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:40:00 +0500

    easy absolute-sum

--- easy/absolute-sum/code.js
+++ easy/absolute-sum/code.js
@@ -1,5 +1,16 @@
 function getAbsSum(arr) {
   // Your code here.
+  let sum = 0;
+  // Yig'indini saqlash uchun boshlang'ich qiymat
+
+  for (let i = 0; i < arr.length; i++) {
+    // Massivni boshidan oxirigacha aylanamiz
+    sum += Math.abs(arr[i]);
+    // Har bir elementning absolyut qiymatini yig'indiga qo'shamiz
+  }
+
+  // Yakuniy natijani return qilamiz
+  return sum;
 }
 
 module.exports = getAbsSum;
