c4335f1d0b52dcbdd74aa380377e3757a8647808
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:43:00 +0500

    easy calculate-the-mean

--- easy/calculate-the-mean/code.js
+++ easy/calculate-the-mean/code.js
@@ -1,5 +1,18 @@
 function mean(arr) {
   // Your code here.
+  let sum = 0;
+  // Yig'indini saqlash uchun boshlang'ich qiymat
+
+  for (let i = 0; i < arr.length; i++) {
+    // Massivdagi barcha sonlarni qo'shamiz
+    sum += arr[i];
+  }
+
+  // O'rtacha qiymatni hisoblaymiz
+  let average = sum / arr.length;
+
+  // Natijani 2 ta kasr xonasigacha yaxlitlab qaytaramiz
+  return Number(average.toFixed(2));
 }
 
 module.exports = mean;
