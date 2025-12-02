143dba085e4f8c04b33152a95089b9ae1815909c
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:46:00 +0500

    easy chat-room-status

--- easy/chat-room-status/code.js
+++ easy/chat-room-status/code.js
@@ -1,5 +1,29 @@
 function chatroomStatus(users) {
   // Your code here.
+  // Agar chat bo'sh bo'lsa
+  if (users.length === 0) {
+    return "no one online";
+  }
+
+  // Agar 1 ta foydalanuvchi bo'lsa
+  if (users.length === 1) {
+    return users[0] + " online";
+  }
+
+  // Agar 2 ta foydalanuvchi bo'lsa
+  if (users.length === 2) {
+    return users[0] + " and " + users[1] + " online";
+  }
+
+  // Agar 2 tadan ko'p foydalanuvchi bo'lsa
+  return (
+    users[0] +
+    ", " +
+    users[1] +
+    " and " +
+    (users.length - 2) +
+    " more online"
+  );
 }
 
 module.exports = chatroomStatus;
