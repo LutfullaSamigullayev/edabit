71039da31c58105649d16058c5f23f5373c5a84f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:41:00 +0500

    easy add-up-the-numbers-from-a-single-number

--- easy/add-up-the-numbers-from-a-single-number/code.js
+++ easy/add-up-the-numbers-from-a-single-number/code.js
@@ -1,5 +1,16 @@
 function addUp(num) {
   // Your code here.
+  let sum = 0;
+  // Yig'indini saqlash uchun boshlang'ich qiymat
+
+  for (let i = 1; i <= num; i++) {
+    // 1 dan n gacha bo'lgan sonlarni aylanamiz
+    sum += i;
+    // Har bir sonni yig'indiga qo'shamiz
+  }
+
+  // Yakuniy natijani return qilamiz
+  return sum;
 }
 
 module.exports = addUp;
