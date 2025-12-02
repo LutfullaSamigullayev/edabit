986bd9abbc617850f9bf99d0003a6ebb8c27d770
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-02 17:45:00 +0500

    easy capitalize-by-ascii

--- easy/capitalize-by-ascii/code.js
+++ easy/capitalize-by-ascii/code.js
@@ -1,5 +1,27 @@
 function asciiCapitalize(str) {
   // Your code here.
+  let result = "";
+  // Natijani yig'ish uchun bo'sh string
+
+  for (let i = 0; i < str.length; i++) {
+    // Har bir belgini navbatma-navbat olamiz
+    const char = str[i];
+    const code = char.charCodeAt(0);
+    // Belgining ASCII kodini aniqlaymiz
+
+    if (code % 2 === 0) {
+      // Agar ASCII kodi juft bo'lsa
+      result += char.toUpperCase();
+      // Katta harfga o'tkazamiz
+    } else {
+      // Agar ASCII kodi toq bo'lsa
+      result += char.toLowerCase();
+      // Kichik harfga o'tkazamiz
+    }
+  }
+
+  // Yakuniy stringni return qilamiz
+  return result;
 }
 
 module.exports = asciiCapitalize;
