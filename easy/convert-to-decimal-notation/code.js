f16ee14d9ed62be224a4460fe979c9e9d5e5008a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-05 17:50:00 +0500

    easy convert-to-decimal-notation

--- easy/convert-to-decimal-notation/code.js
+++ easy/convert-to-decimal-notation/code.js
@@ -1,5 +1,12 @@
 function convertToDecimal(perc) {
   // Your code here.
+  return perc.map(item => {
+    // % belgisini olib tashlab, son holatiga o‘tkazamiz
+    const num = parseFloat(item.replace("%", ""));
+
+    // Foizni o‘nlik kasrga aylantiramiz
+    return num / 100;
+  });
 }
 
 module.exports = convertToDecimal;
