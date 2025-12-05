3e25502c84f56ac3eac7a30ca4129aba0c7503b2
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-05 17:51:00 +0500

    easy count-instances-of-a-character-in-a-string

--- easy/count-instances-of-a-character-in-a-string/code.js
+++ easy/count-instances-of-a-character-in-a-string/code.js
@@ -1,5 +1,19 @@
 function charCount(myChar, str) {
   // Your code here.
+  let count = 0;
+  // nechta moslik borligini sanash uchun o'zgaruvchi
+
+  for (let i = 0; i < str.length; i++) {
+    // stringni boshidan oxirigacha aylanamiz
+    if (str[i] === myChar) {
+      // agar belgi mos kelsa
+      count++;
+      // sanagichni bittaga oshiramiz
+    }
+  }
+
+  // yakuniy natijani qaytaramiz
+  return count;
 }
 
 module.exports = charCount;
