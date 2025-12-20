14c85f5db6d0e1943afccd29abb808b41abecb23
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:30:00 +0500

    easy count-ones-in-a-2d-array

--- easy/count-ones-in-a-2d-array/code.js
+++ easy/count-ones-in-a-2d-array/code.js
@@ -1,5 +1,22 @@
 function countOnes(matrix) {
   // Your code here.
+  let count = 0;
+  // nechta 1 borligini sanash uchun o'zgaruvchi
+
+  for (let i = 0; i < matrix.length; i++) {
+    // tashqi massiv bo'ylab aylanamiz
+    for (let j = 0; j < matrix[i].length; j++) {
+      // ichki massiv elementlarini tekshiramiz
+      if (matrix[i][j] === 1) {
+        // agar qiymat 1 bo'lsa
+        count++;
+        // sanagichni oshiramiz
+      }
+    }
+  }
+
+  // yakuniy natijani qaytaramiz
+  return count;
 }
 
 module.exports = countOnes;
