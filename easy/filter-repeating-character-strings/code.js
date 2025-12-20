9eff79593028f9641bcf87e72bf790594d3532aa
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:50:00 +0500

    easy filter-repeating-character-strings

--- easy/filter-repeating-character-strings/code.js
+++ easy/filter-repeating-character-strings/code.js
@@ -1,5 +1,7 @@
 function identicalFilter(arr) {
   // Your code here.
+  // Har bir satrdagi unikal belgilar sonini tekshiramiz
+  return arr.filter(str => new Set(str).size === 1);
 }
 
 module.exports = identicalFilter;
