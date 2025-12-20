4b806032d3087dbfa4147a18592859b83323a074
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 21:00:00 +0500

    easy find-the-minimum-maximum-length-and-average-values

--- easy/find-the-minimum-maximum-length-and-average-values/code.js
+++ easy/find-the-minimum-maximum-length-and-average-values/code.js
@@ -1,5 +1,19 @@
 function minMaxLengthAverage(arr) {
   // Your code here.
+  // 1. Minimum qiymatni topish
+  const min = Math.min(...arr);
+
+  // 2. Maximum qiymatni topish
+  const max = Math.max(...arr);
+
+  // 3. Uzunlik (elementlar soni)
+  const length = arr.length;
+
+  // 4. O‘rtacha qiymatni hisoblash
+  const average = arr.reduce((sum, num) => sum + num, 0) / length;
+
+  // Natijani massiv ko‘rinishida qaytaramiz
+  return [min, max, length, average];
 }
 
 module.exports = minMaxLengthAverage;
