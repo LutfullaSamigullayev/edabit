864f8a7df41b92f617b3c61b7de2b94ebb57bc1b
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:45:00 +0500

    easy filter-out-strings-from-an-array

--- easy/filter-out-strings-from-an-array/code.js
+++ easy/filter-out-strings-from-an-array/code.js
@@ -1,5 +1,7 @@
 function filterArray(arr) {
   // Your code here.
+   // Faqat number tipidagi elementlarni qaytaradi
+   return arr.filter(item => typeof item === "number");
 }
 
 module.exports = filterArray;
