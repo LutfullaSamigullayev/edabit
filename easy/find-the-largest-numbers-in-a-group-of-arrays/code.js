67650b8db7dc0447af2efd03b80f101ae63e201e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:55:00 +0500

    easy find-the-largest-numbers-in-a-group-of-arrays

--- easy/find-the-largest-numbers-in-a-group-of-arrays/code.js
+++ easy/find-the-largest-numbers-in-a-group-of-arrays/code.js
@@ -1,5 +1,11 @@
 function findLargestNums(arr) {
   // Your code here.
+  // Har bir ichki massivni map() orqali aylantiramiz
+  return arr.map(innerArr => {
+    // Math.max() eng katta sonni topadi
+    // ...innerArr → spread operator, massiv elementlarini alohidaargumentga aylantiradi
+    return Math.max(...innerArr);
+  });
 }
 
 module.exports = findLargestNums;
