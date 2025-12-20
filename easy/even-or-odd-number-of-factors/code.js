6fef6a774d1f83d78bc75e50d617812cbe2f0473
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:35:00 +0500

    easy even-or-odd-number-of-factors

--- easy/even-or-odd-number-of-factors/code.js
+++ easy/even-or-odd-number-of-factors/code.js
@@ -1,5 +1,16 @@
 function factorGroup(num) {
   // Your code here.
+  // Agar n mukammal kvadrat bo'lsa (masalan 36 -> 6*6)
+  // Unda bo'luvchilar soni toq bo'ladi
+  const root = Math.sqrt(num);
+
+  if (Number.isInteger(root)) {
+    // Mukammal kvadrat: bo'luvchilar soni toq
+    return "odd";
+  }
+
+  // Aks holda bo'luvchilar soni juft
+  return "even";
 }
 
 module.exports = factorGroup;
