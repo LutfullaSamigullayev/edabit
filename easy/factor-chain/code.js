a9b8240a5e205454ed4362cfe6a1eb09018310d3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 20:40:00 +0500

    easy factor-chain

--- easy/factor-chain/code.js
+++ easy/factor-chain/code.js
@@ -1,5 +1,15 @@
 function factorChain(arr) {
   // Your code here.
+   // Har bir element keyingi elementning bo'luvchisi bo'ladimi?
+   for (let i = 0; i < arr.length - 1; i++) {
+    // Agar keyingi element hozirgi elementga karrali bo'lmasa, chain emas
+    if (arr[i + 1] % arr[i] !== 0) {
+      return false;
+    }
+  }
+
+  // Barcha shartlar bajarildi
+  return true;
 }
 
 module.exports = factorChain;
