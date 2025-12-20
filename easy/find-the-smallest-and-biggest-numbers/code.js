eb6b345a7502ee3af02415ab5fc659745fbe8a5f
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-20 21:05:00 +0500

    easy find-the-smallest-and-biggest-numbers

--- easy/find-the-smallest-and-biggest-numbers/code.js
+++ easy/find-the-smallest-and-biggest-numbers/code.js
@@ -1,5 +1,12 @@
 function minMax(arr) {
   // Your code here.
+  // Eng kichik qiymatni topamiz
+  const min = Math.min(...arr);
+  // Eng katta qiymatni topamiz
+  const max = Math.max(...arr);
+
+  // Ikkalasini massiv qilib qaytaramiz
+  return [min, max];
 }
 
 module.exports = minMax;
